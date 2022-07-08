4e55ec3b7fe924ebdfb3243a8b99e24c89279124
heaven00 <69586104+lhb8106@users.noreply.github.com>
2022-07-08 14:49:11 +0900

    [FEAT] #5 - make bottom navi

--- app/src/main/java/com/mument_android/app/presentation/ui/main/MainActivity.kt
+++ app/src/main/java/com/mument_android/app/presentation/ui/main/MainActivity.kt
@@ -2,11 +2,19 @@ package com.mument_android.app.presentation.ui.main
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.navigation.findNavController
+import androidx.navigation.ui.NavigationUI
 import com.mument_android.R
+import com.mument_android.app.presentation.base.BaseActivity
+import com.mument_android.databinding.ActivityMainBinding
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : BaseActivity<ActivityMainBinding>(R.layout.activity_main) {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        initNavigation()
+    }
+
+    private fun initNavigation() {
+        NavigationUI.setupWithNavController(binding.navBar, findNavController(R.id.nav_host))
     }
 }
