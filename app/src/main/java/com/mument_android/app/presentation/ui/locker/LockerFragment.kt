b2d59ff556ead44feadd7598f4999d03b49df011
heaven00 <69586104+lhb8106@users.noreply.github.com>
2022-07-08 14:08:52 +0900

    [FEAT] #5 - add fragment

---
+++ app/src/main/java/com/mument_android/app/presentation/ui/locker/LockerFragment.kt
@@ -0,0 +1,26 @@
+package com.mument_android.app.presentation.ui.locker
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.mument_android.R
+import com.mument_android.app.util.AutoClearedValue
+import com.mument_android.databinding.FragmentLockerBinding
+
+class LockerFragment : Fragment() {
+   private var binding by AutoClearedValue<FragmentLockerBinding>()
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View = FragmentLockerBinding.inflate(inflater, container, false). run {
+        binding = this
+        this.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+    }
+}
