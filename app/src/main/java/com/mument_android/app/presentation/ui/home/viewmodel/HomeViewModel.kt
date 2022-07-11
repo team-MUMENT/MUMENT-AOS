3147211a453daa7ce0639605620e5346bba3d6b5
sonpyeonghwa <ssrsph@naver.com>
2022-07-11 17:57:39 +0900

    [FIX] 12 - tag 관련 수정

--- app/src/main/java/com/mument_android/app/presentation/ui/home/viewmodel/HomeViewModel.kt
+++ app/src/main/java/com/mument_android/app/presentation/ui/home/viewmodel/HomeViewModel.kt
@@ -1,4 +1,10 @@
 package com.mument_android.app.presentation.ui.home.viewmodel
 
-class HomeViewModel {
+import androidx.lifecycle.ViewModel
+import com.mument_android.R
+import com.mument_android.app.data.EmotionalTag.Companion.findEmotionalTag
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.asStateFlow
+
+class HomeViewModel: ViewModel() {
 }
