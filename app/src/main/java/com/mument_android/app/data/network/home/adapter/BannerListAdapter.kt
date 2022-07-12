b2bba57c9a37421ec9925afe135ae54360558310
minho <my_alsgh@naver.com>
2022-07-13 01:13:00 +0900

    [Chore] Spannable Text Complete HomeViewModel 30

--- app/src/main/java/com/mument_android/app/data/network/home/adapter/BannerListAdapter.kt
+++ app/src/main/java/com/mument_android/app/data/network/home/adapter/BannerListAdapter.kt
@@ -1,10 +1,17 @@
 package com.mument_android.app.data.network.home.adapter
 
+import android.graphics.Color
+import android.text.SpannableString
+import android.text.SpannableStringBuilder
+import android.text.Spanned
+import android.text.style.ForegroundColorSpan
+import android.text.style.StyleSpan
 import android.view.LayoutInflater
 import android.view.ViewGroup
 import androidx.recyclerview.widget.ListAdapter
 import androidx.recyclerview.widget.RecyclerView
 import com.mument_android.BR
+import com.mument_android.R
 import com.mument_android.app.domain.entity.TempBannerData
 import com.mument_android.app.util.GlobalDiffCallBack
 import com.mument_android.databinding.ItemBannerLayoutBinding
@@ -27,6 +34,10 @@ class BannerListAdapter :
     }
 
     override fun onBindViewHolder(holder: BannerViewHolder, position: Int) {
+        val builder = SpannableStringBuilder((position+1).toString()+" / "+itemCount)
+        val span = ForegroundColorSpan(Color.WHITE)
+        builder.setSpan(span, 0,1,Spanned.SPAN_EXCLUSIVE_INCLUSIVE)
         holder.binding.setVariable(BR.banner, getItem(position))
+        holder.binding.tvIndex.text = builder
     }
 }
