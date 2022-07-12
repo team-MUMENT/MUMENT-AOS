a6d3d855fb07fae74c9ffbf9b41ef4257ebc71dd
heaven00 <69586104+lhb8106@users.noreply.github.com>
2022-07-12 17:40:01 +0900

    [FEAT] #17 - add data

---
+++ app/src/main/java/com/mument_android/app/presentation/ui/locker/adapter/LockerMumentAdapter.kt
@@ -0,0 +1,34 @@
+package com.mument_android.app.presentation.ui.locker.adapter
+
+import android.view.LayoutInflater
+import android.view.ViewGroup
+import androidx.recyclerview.widget.RecyclerView
+import com.mument_android.databinding.ItemLockerDateBinding
+
+
+class LockerMumentAdapter() {}
+/*
+class LockerMumentAdapter : RecyclerView.Adapter<>() {
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): LockerMumentAdapter {
+       val binding = ItemLockerDateBinding.inflate(
+           LayoutInflater.from(parent.context),
+           parent,
+           false
+       )
+
+    }
+
+    override fun onBindViewHolder(holder: LockerMumentAdapter, position: Int) {
+        TODO("Not yet implemented")
+    }
+
+    override fun getItemCount(): Int {
+        TODO("Not yet implemented")
+    }
+
+    inner class MumentViewHolder(
+        val binding: ItemLockerDateBinding
+    ) : RecyclerView.ViewHolder(binding.root) {}
+}
+
+ */
