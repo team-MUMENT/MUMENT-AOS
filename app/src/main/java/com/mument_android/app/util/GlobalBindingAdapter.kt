b9c1239b4f9dd13d87d231d3dbad4e53d566ec4f
minho <my_alsgh@naver.com>
2022-07-12 16:02:28 +0900

    rebase develop

--- app/src/main/java/com/mument_android/app/util/GlobalBindingAdapter.kt
+++ app/src/main/java/com/mument_android/app/util/GlobalBindingAdapter.kt
@@ -22,25 +22,22 @@ object GlobalBindingAdapter {
     @BindingAdapter("load_profile")
     fun loadProfileImage(view: ImageView, url: String) {
         if (url.isNotBlank()) {
-            CoroutineScope(Dispatchers.IO).launch {
                 view.load(url) {
                     crossfade(true)
                     this.transformations(CircleCropTransformation())
                 }
-            }
         } else {
             TODO("placeholder")
         }
     }
+
     @JvmStatic
     @BindingAdapter("load_album")
     fun loadAlbumImage(view: ImageView, url: String) {
         if (url.isNotBlank()) {
-            CoroutineScope(Dispatchers.IO).launch {
                 view.load(url) {
                     crossfade(true)
                     this.transformations(RoundedCornersTransformation(11.0f))
-                }
             }
         } else {
             TODO("placeholder")
@@ -62,4 +59,16 @@ object GlobalBindingAdapter {
     @JvmStatic
     @InverseBindingAdapter(attribute = "selectedTag", event = "checkedChanged")
     fun EmotionalTagCheckBox.getIsChecked():Boolean? = isChecked.value
+
+    @BindingAdapter("load_album_top")
+    fun loadAlbumTopImage(view: ImageView, url: String) {
+        if (url.isNotBlank()) {
+                view.load(url) {
+                    crossfade(true)
+                    this.transformations(RoundedCornersTransformation(11.0f, 11.0f))
+            }
+        } else {
+            TODO("placeholder")
+        }
+    }
 }
