9b1978e7aa2bfef19d49e359c77dd90c286fd653
jinsilWoo <wryt9224@gmail.com>
2022-07-13 18:15:20 +0900

    [FEAT] #20 selector 구현

--- app/src/main/java/com/mument_android/app/presentation/ui/record/RecordFragment.kt
+++ app/src/main/java/com/mument_android/app/presentation/ui/record/RecordFragment.kt
@@ -4,10 +4,11 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.core.content.ContextCompat
+import androidx.core.view.updateMargins
 import androidx.fragment.app.Fragment
-import com.google.android.flexbox.FlexDirection
-import com.google.android.flexbox.FlexWrap
-import com.google.android.flexbox.FlexboxLayoutManager
+import com.google.android.flexbox.*
+import com.mument_android.R
 import com.mument_android.app.data.enumtype.EmotionalTag
 import com.mument_android.app.util.AutoClearedValue
 import com.mument_android.databinding.FragmentRecordBinding
@@ -28,20 +29,22 @@ class RecordFragment : Fragment() {
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
         setEmotionalList()
+        clickEvent()
     }
 
     private fun setEmotionalList() {
         with(binding.rvRecordEmotionalTags) {
             adapter = RecordTagAdapter()
-
             FlexboxLayoutManager(context).apply{
                 flexWrap = FlexWrap.WRAP
                 flexDirection = FlexDirection.ROW
+
 
             }.let {
                 binding.rvRecordEmotionalTags.layoutManager =it
                 binding.rvRecordEmotionalTags.adapter = recordTagAdapter
             }
+
             (adapter as RecordTagAdapter).submitList(EmotionalTag.values().map { it.tag })
 
 //            EmotionalTag.values().map { requireContext().getString(it.tag) }.forEach {
@@ -60,12 +63,35 @@ class RecordFragment : Fragment() {
                 binding.rvRecordEmotionalTags2.layoutManager =it
                 binding.rvRecordEmotionalTags2.adapter = recordTagAdapter
             }
+
+            FlexboxLayoutManager(context).apply {
+                alignItems = AlignItems.FLEX_START
+
+            }.also { binding.rvRecordEmotionalTags2.layoutManager = it }
+            val itemDecoration = FlexboxItemDecoration(context).apply {
+                setDrawable(ContextCompat.getDrawable(context, R.drawable.rectangle_fill_blue3_20dp))
+                setOrientation(FlexboxItemDecoration.HORIZONTAL)
+            }
+            if (binding.rvRecordEmotionalTags2.itemDecorationCount == 0) {
+                binding.rvRecordEmotionalTags2.addItemDecoration(itemDecoration)
+            }
             (adapter as RecordTagAdapter).submitList(EmotionalTag.values().map { it.tag })
 
-//            EmotionalTag.values().map { requireContext().getString(it.tag) }.forEach {
-//                Timber.e("tag: $it")
-//            }
         }
 
     }
+
+    private fun clickEvent(){
+        binding.btnRecordFirst.isSelected =true
+        binding.btnRecordFirst.setOnClickListener {
+            binding.btnRecordFirst.isSelected =true
+            binding.btnRecordSecond.isSelected =false
+
+        }
+         binding.btnRecordSecond.setOnClickListener{
+             binding.btnRecordFirst.isSelected =false
+             binding.btnRecordSecond.isSelected =true
+         }
+    }
+
 }
--- app/src/main/java/com/mument_android/app/presentation/ui/record/viewmodel/RecodeViewModel.kt
+++ app/src/main/java/com/mument_android/app/presentation/ui/record/viewmodel/RecodeViewModel.kt
@@ -1,5 +1,7 @@
 package com.mument_android.app.presentation.ui.record.viewmodel
 
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 import com.mument_android.R
 import com.mument_android.app.data.enumtype.EmotionalTag
@@ -7,5 +9,12 @@ import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.asStateFlow
 
 class RecodeViewModel :ViewModel(){
-
+//    private val _clickEvent = MutableLiveData<Boolean>()
+//    val clickEvent= _clickEvent
+//
+//
+//    fun btnClickEvent(){
+//        _clickEvent.value = true
+//        _clickEvent.value != _clickEvent.value
+//    }
 }
