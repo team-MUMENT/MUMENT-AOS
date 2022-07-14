f5ff0ccd4526ac293552ccdd79c9a546cbfdee8d
sonpyeonghwa <ssrsph@naver.com>
2022-07-14 21:42:07 +0900

    [FEAT] #37 - Mument Detail Tag FlexboxLayoutManager:

--- app/src/main/java/com/mument_android/app/presentation/ui/detail/MumentDetailFragment.kt
+++ app/src/main/java/com/mument_android/app/presentation/ui/detail/MumentDetailFragment.kt
@@ -8,6 +8,9 @@ import android.widget.Button
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.viewModels
 import androidx.lifecycle.lifecycleScope
+import com.google.android.flexbox.FlexDirection
+import com.google.android.flexbox.FlexWrap
+import com.google.android.flexbox.FlexboxLayoutManager
 import com.mument_android.app.data.network.util.ApiResult
 import com.mument_android.app.util.AutoClearedValue
 import com.mument_android.app.util.RecyclerviewItemDivider
@@ -37,8 +40,12 @@ class MumentDetailFragment : Fragment() {
             lifecycleOwner = viewLifecycleOwner
             mumentDetailViewModel= viewModel
 
-            rvEmotionalTags.adapter = EmotionalTagListAdapter()
-            rvEmotionalTags.addItemDecoration(RecyclerviewItemDivider(8.dpToPx(requireContext()),0))
+            rvMumentTags.adapter = EmotionalTagListAdapter()
+            rvMumentTags.layoutManager = FlexboxLayoutManager(requireContext()).apply {
+                flexWrap = FlexWrap.WRAP
+                flexDirection = FlexDirection.ROW
+            }
+            rvMumentTags.addItemDecoration(RecyclerviewItemDivider(7.dpToPx(requireContext()), 5.dpToPx(requireContext())))
         }
 
         setMumentTagList()
@@ -50,7 +57,7 @@ class MumentDetailFragment : Fragment() {
                 is ApiResult.Loading -> {}
                 is ApiResult.Failure -> {}
                 is ApiResult.Success -> {
-                    (binding.rvEmotionalTags.adapter as EmotionalTagListAdapter).submitList(result.data?.combineTags())
+                    (binding.rvMumentTags.adapter as EmotionalTagListAdapter).submitList(result.data?.combineTags())
                 }
             }
         }
--- app/src/main/java/com/mument_android/app/presentation/ui/record/RecordTagAdapter.kt
+++ app/src/main/java/com/mument_android/app/presentation/ui/record/RecordTagAdapter.kt
@@ -5,12 +5,13 @@ import android.view.ViewGroup
 import androidx.recyclerview.widget.ListAdapter
 import androidx.recyclerview.widget.RecyclerView
 import com.mument_android.BR
+import com.mument_android.app.domain.entity.TagEntity
 import com.mument_android.app.util.GlobalDiffCallBack
 import com.mument_android.databinding.ItemEmotionalTagBinding
 
 
-class RecordTagAdapter: ListAdapter<Int, RecordTagAdapter.RecordTagViewHolder>(
-    GlobalDiffCallBack<Int>()
+class RecordTagAdapter: ListAdapter<TagEntity, RecordTagAdapter.RecordTagViewHolder>(
+    GlobalDiffCallBack<TagEntity>()
 ) {
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): RecordTagViewHolder {
         val binding = ItemEmotionalTagBinding.inflate(LayoutInflater.from(parent.context), parent, false)
@@ -18,7 +19,7 @@ class RecordTagAdapter: ListAdapter<Int, RecordTagAdapter.RecordTagViewHolder>(
     }
 
     override fun onBindViewHolder(holder: RecordTagViewHolder, position: Int) {
-        holder.binding.setVariable(BR.tagData, getItem(position))
+        holder.binding.setVariable(BR.tagEntity, getItem(position))
     }
 
     class RecordTagViewHolder(val binding: ItemEmotionalTagBinding):RecyclerView.ViewHolder(binding.root){
