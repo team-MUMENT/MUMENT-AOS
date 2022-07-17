3688c94ad3363781b856d70b66559d4370e09b25
sonpyeonghwa <ssrsph@naver.com>
2022-07-17 23:41:32 +0900

    [FEAT] View Visibility Animation 추가

--- app/src/main/java/com/mument_android/app/util/Extensions.kt
+++ app/src/main/java/com/mument_android/app/util/Extensions.kt
@@ -2,10 +2,16 @@ package com.mument_android.app.util
 
 import android.app.Activity
 import android.util.DisplayMetrics
+import android.view.View
+import android.view.animation.AlphaAnimation
+import android.view.animation.Animation
+import android.view.animation.AnimationSet
+import android.view.animation.TranslateAnimation
 import androidx.lifecycle.LifecycleCoroutineScope
 import androidx.lifecycle.LifecycleOwner
 import kotlinx.coroutines.flow.StateFlow
 import kotlinx.coroutines.flow.collect
+import java.lang.IllegalArgumentException
 
 fun <T>StateFlow<T>.launchWhenCreated(lifecycleScope: LifecycleCoroutineScope, callback: (T) -> Unit) {
     lifecycleScope.launchWhenCreated { collect { callback(it) } }
@@ -18,6 +24,4 @@ fun <T>StateFlow<T>.launchWhenStarted(lifecycleScope: LifecycleCoroutineScope, c
 fun <T>StateFlow<T>.launchWhenResumed(lifecycleScope: LifecycleCoroutineScope, callback: (T) -> Unit) {
     lifecycleScope.launchWhenResumed { collect { callback(it) } }
 }
-
-
 
