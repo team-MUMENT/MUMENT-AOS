5726076eba5ce220b92bfed836f1c252e70c78a8
heaven00 <69586104+lhb8106@users.noreply.github.com>
2022-07-18 07:11:55 +0900

    [FEAT] #70 - like adapter

--- app/src/main/java/com/mument_android/app/presentation/ui/locker/MyLikeFragment.kt
+++ app/src/main/java/com/mument_android/app/presentation/ui/locker/MyLikeFragment.kt
@@ -5,11 +5,17 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import androidx.fragment.app.viewModels
+import androidx.lifecycle.lifecycleScope
+import com.mument_android.app.presentation.ui.locker.adapter.LockerTimeAdapter
+import com.mument_android.app.presentation.ui.locker.viewmodel.LockerViewModel
 import com.mument_android.app.util.AutoClearedValue
+import com.mument_android.app.util.launchWhenCreated
 import com.mument_android.databinding.FragmentMyLikeBinding
 
 class MyLikeFragment : Fragment() {
     private var binding by AutoClearedValue<FragmentMyLikeBinding>()
+    private val viewModel: LockerViewModel by viewModels( ownerProducer = { requireParentFragment() } )
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
@@ -21,5 +27,20 @@ class MyLikeFragment : Fragment() {
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
+        binding.lifecycleOwner = viewLifecycleOwner
+        binding.viewModel = viewModel
+        setMyMumentListAdapter()
+    }
+
+    private fun setMyMumentListAdapter() {
+        binding.rvLikeLinear.run {
+            viewModel.myMuments.observe(viewLifecycleOwner) {
+                (adapter as LockerTimeAdapter).submitList(it)
+            }
+            viewModel.isGridLayout.launchWhenCreated(viewLifecycleOwner.lifecycleScope) { isGridLayout ->
+                adapter = LockerTimeAdapter(isGridLayout)
+                (adapter as LockerTimeAdapter).submitList(viewModel.myMuments.value)
+            }
+        }
     }
 }
