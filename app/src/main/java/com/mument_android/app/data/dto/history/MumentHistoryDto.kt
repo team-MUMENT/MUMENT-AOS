b9eae876efd32df0e91d49c0c0d397d901b82379
minho <my_alsgh@naver.com>
2022-07-20 07:56:04 +0900

    [Chore] Dto Setting
    71

---
+++ app/src/main/java/com/mument_android/app/data/dto/history/MumentHistoryDto.kt
@@ -0,0 +1,6 @@
+package com.mument_android.app.data.dto.history
+
+data class MumentHistoryDto(
+    val mumentHistory: List<MumentHistory>?,
+    val music: MusicX
+)
---
+++ app/src/main/java/com/mument_android/app/data/dto/history/Music.kt
@@ -0,0 +1,5 @@
+package com.mument_android.app.data.dto.history
+
+data class Music(
+    val _id: String
+)
