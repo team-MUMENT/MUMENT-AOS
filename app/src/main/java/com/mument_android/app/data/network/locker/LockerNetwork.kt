ea5e3ad39e546c0340f5c53cd161a6e2e1aa49d9
heaven00 <69586104+lhb8106@users.noreply.github.com>
2022-07-20 14:56:51 +0900

    [FEAT] #69 - add api

--- app/src/main/java/com/mument_android/app/data/network/locker/LockerNetwork.kt
+++ app/src/main/java/com/mument_android/app/data/network/locker/LockerNetwork.kt
@@ -2,8 +2,11 @@ package com.mument_android.app.data.network.locker
 
 import com.mument_android.app.data.dto.ResponseMyMumentListDto
 import retrofit2.http.GET
+import retrofit2.http.Path
 
 interface LockerNetwork {
-    @GET("")
-    suspend fun fetchLockerMumentList(): ResponseMyMumentListDto
+    @GET("/user/my/{userId}/list")
+    suspend fun lockerMumentList(
+        @Path("userId") userId : String
+    ): ResponseMyMumentListDto
 }
