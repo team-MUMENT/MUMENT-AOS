a581aa5ff829ca0708544546f1f04fd09c35b43d
heaven00 <69586104+lhb8106@users.noreply.github.com>
2022-07-21 23:53:04 +0900

    [FEAT] #90 - delete unuseful code

--- app/src/main/java/com/mument_android/app/presentation/ui/locker/MyMumentFragment.kt
+++ app/src/main/java/com/mument_android/app/presentation/ui/locker/MyMumentFragment.kt
@@ -57,18 +57,11 @@ class MyMumentFragment : Fragment() {
         }
     }
 
-    override fun onResume() {
-        super.onResume()
-        setMyMumentListAdapter()
-    }
-
     private fun setMyMumentListAdapter() {
         setGridServerConnection()
         lockerViewModel.myMuments.launchWhenCreated(viewLifecycleOwner.lifecycleScope){
             when(it){
-                is ApiResult.Loading -> {
-
-                }
+                is ApiResult.Loading -> {}
                 is ApiResult.Failure -> {}
                 is ApiResult.Success -> {
                     binding.rvMumentLinear.adapter = LockerTimeAdapter(false)
