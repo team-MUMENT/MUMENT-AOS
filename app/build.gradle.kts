981fdc788664feb3e9c11cc15041866bfb9b2ff5
minho <my_alsgh@naver.com>
2022-09-05 23:20:48 +0900

    [Feat] Add Dependency Kts Style

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -52,75 +52,26 @@ android {
 }
 
 dependencies {
-
-    implementation(AndroidXDependencies.coreKtx)
-    implementation(AndroidXDependencies.appCompat)
-    implementation(MaterialDesignDependencies.materialDesign)
-    implementation(AndroidXDependencies.constraintLayout)
+    addAndroidXDependencies()
     implementation(AndroidXDependencies.window)
-    testImplementation(TestDependencies.jUnit)
-    androidTestImplementation(TestDependencies.androidTest)
-    androidTestImplementation(TestDependencies.espresso)
-
-    //navigation component
-    implementation(AndroidXDependencies.navigationFragment)
-    implementation(AndroidXDependencies.navigationUI)
-
-    //coil
+    addNetworkDependencies()
+    addNavigationDependencies()
+    addDaggerHiltDependencies()
+    implementation(AndroidXDependencies.lifecycleJava8)
+    addLifecycleDependencies()
+    addRoomDependencies()
+    addTestDependencies()
     implementation(ThirdPartyDependencies.coil)
-
-    //Lottie
     implementation(ThirdPartyDependencies.lottie)
-
-    //cardView
     implementation(AndroidXDependencies.cardview)
-
-    //coRoutine
     implementation(KotlinDependencies.coroutines)
-
-    //retrofit
-    implementation(ThirdPartyDependencies.gsonConverter)
-    implementation(ThirdPartyDependencies.retrofit)
-
-    //okhttp
-    implementation(ThirdPartyDependencies.okHttp)
-    implementation(ThirdPartyDependencies.okHttpBom)
-    implementation(ThirdPartyDependencies.okHttpLoggingInterceptor)
-
-    //gson
+    implementation(AndroidXDependencies.coroutines)
     implementation(ThirdPartyDependencies.gson)
     implementation(KotlinDependencies.kotlinxSerialization)
-
-    //Timber
     implementation(ThirdPartyDependencies.timber)
-
-    //recyclerview
-    implementation(AndroidXDependencies.recyclerView)
-
-    // Jetpack Lifecycle
-    implementation(AndroidXDependencies.coroutines)
-    implementation(AndroidXDependencies.lifeCycleKtx)
-    implementation(AndroidXDependencies.lifecycleJava8)
-
-    // Hilt
-    implementation(HiltDependencies.hilt)
-    kapt(KaptDependencies.hiltAndroidCompiler)
-
-    implementation(TestDependencies.mockito)
-    androidTestImplementation(TestDependencies.mockitoAndroidTest)
-
-    testImplementation(TestDependencies.coroutinesTest)
-    androidTestImplementation(TestDependencies.coreTesting)
-    testImplementation(TestDependencies.coreTesting)
-
-    // Room
-    kapt(AndroidXDependencies.roomCompiler)
-    implementation(AndroidXDependencies.room)
-    implementation(AndroidXDependencies.roomRuntime)
-
-    // DataStore
-    /*implementation(AndroidXDependencies.dataStore)
-    implementation(AndroidXDependencies.protoBuf)*/
-    //flexBox
     implementation(ThirdPartyDependencies.flexBox)
+
+    implementation(project(Modules.CORE_MODULE))
+    implementation(project(Modules.DATA_MODULE))
+    implementation(project(Modules.DOMAIN_MODULE))
 }
