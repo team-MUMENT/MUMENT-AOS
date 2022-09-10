9365a291e0f954f0d8a5ce501aa0948b1a560d6b
minho <my_alsgh@naver.com>
2022-09-10 21:56:32 +0900

    [Chore] Not Use Code Remove Suggest

--- app/src/main/java/com/mument_android/app/application/MumentApplication.kt
+++ app/src/main/java/com/mument_android/app/application/MumentApplication.kt
@@ -12,11 +12,11 @@ class MumentApplication: Application() {
     override fun onCreate() {
         super.onCreate()
         if (BuildConfig.DEBUG) Timber.plant(Timber.DebugTree())
-        val fontRequest = FontRequest(
+        /*val fontRequest = FontRequest( TODO 안 쓰면 지우기
             "com.google.android.gms.fonts",
             "com.google.android.gms",
             "Noto Color Emoji Compat",
-            R.array.com_google_android_gms_fonts_certs)
+            R.array.com_google_android_gms_fonts_certs)*/
 //
 //        val config = FontRequestEmojiCompatConfig(this, fontRequest)
 //        EmojiCompat.init(config)
