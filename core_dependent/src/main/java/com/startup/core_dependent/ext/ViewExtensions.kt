9b11782472eb5e2be062efecf1818cdc30e523ac
minho <my_alsgh@naver.com>
2022-09-19 14:59:21 +0900

    [Feat] core_dependent bindingadapter add

--- core_dependent/src/main/java/com/startup/core_dependent/ext/ViewExtensions.kt
+++ core_dependent/src/main/java/com/startup/core_dependent/ext/ViewExtensions.kt
@@ -1,7 +1,15 @@
 package com.startup.core_dependent.ext
 
+import android.util.TypedValue
 import android.view.View
 import android.widget.TextView
+import androidx.appcompat.widget.AppCompatTextView
+import androidx.core.content.ContextCompat
+import androidx.core.content.res.ResourcesCompat
+import androidx.databinding.BindingAdapter
+import com.startup.core_dependent.R
+import com.startup.core.TagEntity.Companion.TAG_IS_FIRST
+import com.startup.core_dependent.util.ViewUtils.dpToPx
 
 inline fun View.click(crossinline block: () -> Unit) {
     setOnClickListener { block() }
@@ -9,4 +17,21 @@ inline fun View.click(crossinline block: () -> Unit) {
 
 fun TextView.changeTextColor(id: Int) {
     setTextColor(context.getColor(id))
+}
+object BindingExtension{
+
+@JvmStatic
+@BindingAdapter("setTagType")
+fun AppCompatTextView.setTagType(tagType :String){
+
+    val backgroundDrawable =
+        if (tagType == TAG_IS_FIRST) R.drawable.rectangle_fill_purple2_20dp else R.drawable.rectangle_fill_gray5_20dp
+    val textColor =
+        if (tagType == TAG_IS_FIRST) R.color.mument_color_purple1 else R.color.mument_color_gray1
+    background = ContextCompat.getDrawable(context, backgroundDrawable)
+    setTextColor(ContextCompat.getColor(context, textColor))
+    typeface = ResourcesCompat.getFont(context, R.font.notosans_medium)
+    setTextSize(TypedValue.COMPLEX_UNIT_DIP, 12f)
+    setPadding(7.dpToPx(context), 5.dpToPx(context), 7.dpToPx(context), 5.dpToPx(context))
+}
 }
