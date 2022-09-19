b2db1b32cd335ead95d2933dc8aaa3e5af2c8b64
minho <my_alsgh@naver.com>
2022-09-19 14:58:36 +0900

    [Feat] TagEntity Move Core Module

--- core/src/main/java/com/startup/core/TagEntity.kt
+++ core/src/main/java/com/startup/core/TagEntity.kt
@@ -1,14 +1,10 @@
-package com.startup.domain.entity
+package com.startup.core
 
-import android.os.Parcelable
-import kotlinx.parcelize.Parcelize
-
-@Parcelize
 data class TagEntity(
     val tagType: String,
     val tagString: Int,
     val tagIdx: Int
-): Parcelable {
+) {
     companion object {
         const val TAG_IS_FIRST = "TAG_IS_FIRST"
         const val TAG_IMPRESSIVE ="TAG_IMPRESSIVE"
