b8cee2707517e71fc0f6f50a62cb8f8611e89ba5
heaven00 <69586104+lhb8106@users.noreply.github.com>
2022-11-18 12:17:58 +0900

    [FEAT] #124 바텀네비 플로팅 버튼 위치 이동 성공

--- app/src/main/java/com/mument_android/app/presentation/ui/main/MainActivity.kt
+++ app/src/main/java/com/mument_android/app/presentation/ui/main/MainActivity.kt
@@ -7,16 +7,21 @@ import androidx.navigation.findNavController
 import androidx.navigation.fragment.NavHostFragment
 import androidx.navigation.ui.NavigationUI
 import androidx.navigation.ui.setupWithNavController
+import com.google.android.material.bottomappbar.BottomAppBar
+import com.google.android.material.shape.MaterialShapeDrawable
+import com.google.android.material.shape.RelativeCornerSize
+import com.google.android.material.shape.RoundedCornerTreatment
 import com.mument_android.R
-import com.mument_android.domain.entity.detail.MumentDetailEntity
-import com.mument_android.domain.entity.musicdetail.musicdetaildata.Music
 import com.mument_android.app.presentation.ui.detail.mument.navigator.EditMumentNavigator
 import com.mument_android.app.presentation.ui.main.viewmodel.MainViewModel
+import com.mument_android.core_dependent.base.BaseActivity
+import com.mument_android.databinding.ActivityMainBinding
+import com.mument_android.domain.entity.detail.MumentDetailEntity
+import com.mument_android.domain.entity.musicdetail.musicdetaildata.Music
 import com.mument_android.record.RecordFragment.Companion.MUMENT_DETAIL_ENTITY
 import com.mument_android.record.RecordFragment.Companion.MUMENT_ID_FOR_EDIT
-import com.mument_android.databinding.ActivityMainBinding
-import com.mument_android.core_dependent.base.BaseActivity
 import dagger.hilt.android.AndroidEntryPoint
+
 
 @AndroidEntryPoint
 class MainActivity : BaseActivity<ActivityMainBinding>(R.layout.activity_main),
@@ -27,9 +32,21 @@ class MainActivity : BaseActivity<ActivityMainBinding>(R.layout.activity_main),
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         initNavigation()
+        floatingBtn()
+    }
+
+    private fun floatingBtn() {
+        val bottomBarBackground = binding.appBar.background as MaterialShapeDrawable
+        bottomBarBackground.shapeAppearanceModel = bottomBarBackground.shapeAppearanceModel
+            .toBuilder()
+            .setAllCorners(RoundedCornerTreatment()).setAllCornerSizes(RelativeCornerSize(0.5f))
+            .build()
     }
 
     private fun initNavigation() {
+        binding.navBar.background = null
+        binding.navBar.menu.getItem(1).isEnabled = false
+        binding.navBar.menu.getItem(1).isVisible = false
         val navHostFragment =
             supportFragmentManager.findFragmentById(R.id.nav_host) as NavHostFragment
         navController = navHostFragment.navController
