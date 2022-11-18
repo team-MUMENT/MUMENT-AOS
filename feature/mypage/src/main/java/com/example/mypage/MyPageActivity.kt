9abc559487f5e706b1d03b7d4c68405ac6b49474
jinsilWoo <wryt9224@gmail.com>
2022-11-18 22:54:06 +0900

    [FEAT] #128 - Implement MyPage View UI

--- feature/mypage/src/main/java/com/example/mypage/MyPageActivity.kt
+++ feature/mypage/src/main/java/com/example/mypage/MyPageActivity.kt
@@ -1,11 +1,25 @@
 package com.example.mypage
 
+import android.graphics.Paint
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.example.mypage.databinding.ActivityMyPageBinding
 
 class MyPageActivity : AppCompatActivity() {
+    private lateinit var binding : ActivityMyPageBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_my_page)
+
+        binding = ActivityMyPageBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        setTextUnderLine()
     }
+
+    // 텍스트 밑줄 설정하는 함수
+    private fun setTextUnderLine(){
+        binding.tvMyPageLogout.paintFlags = Paint.UNDERLINE_TEXT_FLAG
+        binding.tvMyPageWithdrawal.paintFlags = Paint.UNDERLINE_TEXT_FLAG
+    }
+
 }
