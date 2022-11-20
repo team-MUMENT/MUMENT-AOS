937cda7fa42b6811b2d9d3587384dc98c87e70f6
heaven00 <69586104+lhb8106@users.noreply.github.com>
2022-11-21 01:16:57 +0900

    [FEAT] #130 라디오 버튼 완료 & 체크박스 완료

--- feature/detail/src/main/java/com/mument_android/detail/mument/NotifyMumentActivity.kt
+++ feature/detail/src/main/java/com/mument_android/detail/mument/NotifyMumentActivity.kt
@@ -2,11 +2,129 @@ package com.mument_android.detail.mument
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import com.mument_android.core_dependent.base.BaseActivity
 import com.mument_android.detail.R
+import com.mument_android.detail.databinding.ActivityNotifyMumentBinding
+import dagger.hilt.android.AndroidEntryPoint
 
-class NotifyMumentActivity : AppCompatActivity() {
+@AndroidEntryPoint
+class NotifyMumentActivity :
+    BaseActivity<ActivityNotifyMumentBinding>(R.layout.activity_notify_mument) {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_notify_mument)
+        backBtnListener()
+        checkBoxListener()
+        with(binding) {
+            setRadioBtn(
+                clFirstReason,
+                clSecondReason,
+                clThirdReason,
+                clForthReason,
+                clFifthReason,
+                clSixthReason,
+                clSeventhReason
+            )
+        }
+    }
+
+    private fun backBtnListener() {
+        binding.ivBackButton.setOnClickListener {
+            finish()
+        }
+    }
+
+    private fun setRadioBtn(
+        view1: View,
+        view2: View,
+        view3: View,
+        view4: View,
+        view5: View,
+        view6: View,
+        view7: View
+    ) {
+        view1.setOnClickListener {
+            if (!view1.isSelected) {
+                view1.isSelected = true
+                view2.isSelected = false
+                view3.isSelected = false
+                view4.isSelected = false
+                view5.isSelected = false
+                view6.isSelected = false
+                view7.isSelected = false
+            }
+        }
+        view2.setOnClickListener {
+            if (!view1.isSelected) {
+                view1.isSelected = false
+                view2.isSelected = true
+                view3.isSelected = false
+                view4.isSelected = false
+                view5.isSelected = false
+                view6.isSelected = false
+                view7.isSelected = false
+            }
+        }
+        view3.setOnClickListener {
+            if (!view1.isSelected) {
+                view1.isSelected = false
+                view2.isSelected = false
+                view3.isSelected = true
+                view4.isSelected = false
+                view5.isSelected = false
+                view6.isSelected = false
+                view7.isSelected = false
+            }
+        }
+        view4.setOnClickListener {
+            if (!view1.isSelected) {
+                view1.isSelected = false
+                view2.isSelected = false
+                view3.isSelected = false
+                view4.isSelected = true
+                view5.isSelected = false
+                view6.isSelected = false
+                view7.isSelected = false
+            }
+        }
+        view5.setOnClickListener {
+            if (!view1.isSelected) {
+                view1.isSelected = false
+                view2.isSelected = false
+                view3.isSelected = false
+                view4.isSelected = false
+                view5.isSelected = true
+                view6.isSelected = false
+                view7.isSelected = false
+            }
+        }
+        view6.setOnClickListener {
+            if (!view1.isSelected) {
+                view1.isSelected = false
+                view2.isSelected = false
+                view3.isSelected = false
+                view4.isSelected = false
+                view5.isSelected = false
+                view6.isSelected = true
+                view7.isSelected = false
+            }
+        }
+        view7.setOnClickListener {
+            if (!view1.isSelected) {
+                view1.isSelected = false
+                view2.isSelected = false
+                view3.isSelected = false
+                view4.isSelected = false
+                view5.isSelected = false
+                view6.isSelected = false
+                view7.isSelected = true
+            }
+        }
+    }
+
+    private fun checkBoxListener() {
+        binding.clBlock.setOnClickListener {
+            binding.ivBlockCheck.isSelected = !binding.ivBlockCheck.isSelected
+        }
     }
 }
