79cc58f4653681c5da88dd1ab71c8c5328254a1d
heaven00 <69586104+lhb8106@users.noreply.github.com>
2022-11-20 22:53:15 +0900

    [FEAT] #130 add string

---
+++ feature/detail/src/main/java/com/mument_android/detail/mument/NotifyMumentActivity.kt
@@ -0,0 +1,12 @@
+package com.mument_android.detail.mument
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.mument_android.detail.R
+
+class NotifyMumentActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_notify_mument)
+    }
+}
