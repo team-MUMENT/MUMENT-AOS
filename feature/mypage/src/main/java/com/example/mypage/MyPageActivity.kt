1e74355e11ff67d6eb309d16f0b10cd90b4f7c40
jinsilWoo <wryt9224@gmail.com>
2022-11-21 17:57:01 +0900

    [FEAT]#128-BaseActivity 적용

--- feature/mypage/src/main/java/com/example/mypage/MyPageActivity.kt
+++ feature/mypage/src/main/java/com/example/mypage/MyPageActivity.kt
@@ -1,25 +1,32 @@
 package com.example.mypage
 
 import android.graphics.Paint
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.activity.viewModels
 import com.example.mypage.databinding.ActivityMyPageBinding
+import com.mument_android.core_dependent.base.BaseActivity
 
-class MyPageActivity : AppCompatActivity() {
-    private lateinit var binding : ActivityMyPageBinding
+class MyPageActivity : BaseActivity<ActivityMyPageBinding>(R.layout.activity_my_page) {
+
+    private val myPageViewModel: MyPageViewModel by viewModels()
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
-        binding = ActivityMyPageBinding.inflate(layoutInflater)
+        binding.lifecycleOwner = this
+        binding.myPageViewModel = myPageViewModel
         setContentView(binding.root)
+
 
         setTextUnderLine()
     }
 
     // 텍스트 밑줄 설정하는 함수
-    private fun setTextUnderLine(){
+    private fun setTextUnderLine() {
         binding.tvMyPageLogout.paintFlags = Paint.UNDERLINE_TEXT_FLAG
         binding.tvMyPageWithdrawal.paintFlags = Paint.UNDERLINE_TEXT_FLAG
     }
 
 }
+
+
