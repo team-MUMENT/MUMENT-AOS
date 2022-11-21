883644af0010c28ec18e4007f09168f85d0b94ef
jinsilWoo <wryt9224@gmail.com>
2022-11-22 01:24:40 +0900

    [CHORE]#128- Modify Package File

--- feature/mypage/src/androidTest/java/com/mument_android/mypage/ExampleInstrumentedTest.kt
+++ feature/mypage/src/androidTest/java/com/mument_android/mypage/ExampleInstrumentedTest.kt
@@ -1,4 +1,4 @@
-package com.example.mypage
+package com.mument_android.mypage
 
 import androidx.test.platform.app.InstrumentationRegistry
 import androidx.test.ext.junit.runners.AndroidJUnit4
@@ -19,6 +19,6 @@ class ExampleInstrumentedTest {
     fun useAppContext() {
         // Context of the app under test.
         val appContext = InstrumentationRegistry.getInstrumentation().targetContext
-        assertEquals("com.example.mypage.test", appContext.packageName)
+        assertEquals("com.mument_android.mypage.test", appContext.packageName)
     }
 }
