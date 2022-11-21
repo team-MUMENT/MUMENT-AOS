ca74b20d56645dbef3446b35aae5f4e73e094705
jinsilWoo <wryt9224@gmail.com>
2022-11-22 00:19:45 +0900

    [FEAT]#128- Modify MyPage UI

--- feature/mypage/src/main/java/com/mument_android/mypage/MyPageViewModel.kt
+++ feature/mypage/src/main/java/com/mument_android/mypage/MyPageViewModel.kt
@@ -1,6 +1,5 @@
 package com.mument_android.mypage
 
-import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 import com.mument_android.mypage.data.UserData
@@ -10,9 +9,21 @@ class MyPageViewModel : ViewModel() {
     private val _isBtnClick = MutableLiveData(false)
     val isBtnClick = _isBtnClick
 
+    val userId = MutableLiveData<String>()
+    val userImg = MutableLiveData<Int>()
+
+
     fun isClickBtnEvent(isBtnClick: Boolean) {
         _isBtnClick.value = isBtnClick
     }
 
+    fun fetchUserInfo() {
+        val userData = UserData(
+            userID = userId.value ?: "",
+            userImg = userImg.value ?: 0
+        )
 
+    }
 }
+
+
