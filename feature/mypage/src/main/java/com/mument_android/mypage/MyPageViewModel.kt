8a69be2fb07556535ba0575b6586a5008113f119
jinsilWoo <wryt9224@gmail.com>
2022-11-22 17:50:19 +0900

    [FEAT]#128- Notice UI

--- feature/mypage/src/main/java/com/mument_android/mypage/MyPageViewModel.kt
+++ feature/mypage/src/main/java/com/mument_android/mypage/MyPageViewModel.kt
@@ -2,28 +2,37 @@ package com.mument_android.mypage
 
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
+import com.mument_android.mypage.data.NoticeData
 import com.mument_android.mypage.data.UserData
 
 class MyPageViewModel : ViewModel() {
 
+    //myPage
     private val _isBtnClick = MutableLiveData(false)
     val isBtnClick = _isBtnClick
 
+    //Profile
     val userId = MutableLiveData<String>()
     val userImg = MutableLiveData<Int>()
 
+    //Notice
+    private val _noticeList = MutableLiveData<List<NoticeData>>()
+    val noticeList = _noticeList
 
+
+    //마이페이지 뷰이동 버튼 클릭
     fun isClickBtnEvent(isBtnClick: Boolean) {
         _isBtnClick.value = isBtnClick
     }
 
+    //프로필 유저 정보
     fun fetchUserInfo() {
         val userData = UserData(
             userID = userId.value ?: "",
             userImg = userImg.value ?: 0
         )
-
     }
+
 }
 
 
---
+++ feature/mypage/src/main/java/com/mument_android/mypage/data/NoticeData.kt
@@ -0,0 +1,8 @@
+package com.mument_android.mypage.data
+
+import java.util.*
+
+data class NoticeData(
+    val title : String,
+    val date : Date
+)
