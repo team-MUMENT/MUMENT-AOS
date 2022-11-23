5f64befc5e6abbaf60d1356eb1d103d4c77a49db
jinsilWoo <wryt9224@gmail.com>
2022-11-23 22:43:45 +0900

    [REFACTOR]#128- Refactor Move MyPage to Each View

--- feature/mypage/src/main/java/com/mument_android/mypage/MyPageActivity.kt
+++ feature/mypage/src/main/java/com/mument_android/mypage/MyPageActivity.kt
@@ -5,24 +5,13 @@ import android.os.Bundle
 import android.view.View
 import androidx.activity.viewModels
 import androidx.appcompat.widget.AppCompatImageButton
-import androidx.fragment.app.Fragment
 import com.mument_android.core_dependent.base.BaseActivity
 import com.mument_android.mypage.databinding.ActivityMyPageBinding
+import com.mument_android.mypage.fragment.*
 
 class MyPageActivity : BaseActivity<ActivityMyPageBinding>(R.layout.activity_my_page) {
 
     private val myPageViewModel: MyPageViewModel by viewModels()
-
-    private val fragmentArray: ArrayList<Fragment> = arrayListOf(
-        AlarmSettingFragment(),
-        AlarmSettingFragment(),
-        AlarmSettingFragment(),
-        BlockUserManagementFragment(),
-        AlarmSettingFragment(),
-        BlockUserManagementFragment(),
-        AlarmSettingFragment(),
-        BlockUserManagementFragment()
-    )
 
 
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -42,45 +31,26 @@ class MyPageActivity : BaseActivity<ActivityMyPageBinding>(R.layout.activity_my_
     }
 
     //각 카테고리 버튼 눌렀을 때 이동하는 함수
-    fun transactionBtnEvent(view: View) {
+    fun transactionBtnEvent(transactionView: View) {
 
-        //버튼 Array
-        val btnArray: ArrayList<AppCompatImageButton> = arrayListOf(
-            binding.btnMyPageGoProfile,
-            binding.btnMyPageGoAlarmSetting,
-            binding.btnMyPageGoBlockUserManagement,
-            binding.btnMyPageGoNotice,
-            binding.btnMyPageGoFAQ,
-            binding.btnMyPageGoInquiry,
-            binding.btnMyPageGoAppInfo,
-            binding.btnMyPageGoIntroduceMument
+        val goNextPageBtn = mutableMapOf(
+            binding.btnMyPageGoProfile to ProfileSettingFragment(),
+            binding.btnMyPageGoAlarmSetting to AlarmSettingFragment(),
+            binding.btnMyPageGoBlockUserManagement to BlockUserManagementFragment(),
+            binding.btnMyPageGoNotice to NoticeFragment()
         )
 
-        supportFragmentManager.beginTransaction().add(R.id.fc_my_page, fragmentArray[0])
+        supportFragmentManager.beginTransaction().add(R.id.fc_my_page, ProfileSettingFragment())
             .commit()
         val transaction = supportFragmentManager.beginTransaction()
 
         myPageViewModel.isClickBtnEvent(true)
-        when (view) {
-            btnArray[0] ->
-                transaction.replace(R.id.fc_my_page, fragmentArray[0]).commit()
-            btnArray[1] ->
-                transaction.replace(R.id.fc_my_page, fragmentArray[1]).commit()
-            btnArray[2] ->
-                transaction.replace(R.id.fc_my_page, fragmentArray[2]).commit()
-            btnArray[3] ->
-                transaction.replace(R.id.fc_my_page, fragmentArray[3]).commit()
-            btnArray[4] ->
-                transaction.replace(R.id.fc_my_page, fragmentArray[4]).commit()
-            btnArray[5] ->
-                transaction.replace(R.id.fc_my_page, fragmentArray[5]).commit()
-            btnArray[6] ->
-                transaction.replace(R.id.fc_my_page, fragmentArray[6]).commit()
-            btnArray[7] ->
-                transaction.replace(R.id.fc_my_page, fragmentArray[7]).commit()
+
+        goNextPageBtn.forEach { (btn, view) ->
+            when (transactionView) {
+                btn -> transaction.replace(R.id.fc_my_page, view).commit()
+            }
         }
-
-
     }
 
 }
