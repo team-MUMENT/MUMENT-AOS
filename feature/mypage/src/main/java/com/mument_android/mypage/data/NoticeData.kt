c34ab6122e37ceb0c115d6c5ee63405b220de76d
jinsilWoo <wryt9224@gmail.com>
2022-11-25 22:51:50 +0900

    [CHORE] #128 - Modify import

--- feature/mypage/src/main/java/com/mument_android/mypage/data/NoticeData.kt
+++ feature/mypage/src/main/java/com/mument_android/mypage/data/NoticeData.kt
@@ -1,8 +1,8 @@
 package com.mument_android.mypage.data
 
-import java.util.*
+import java.sql.Date
 
 data class NoticeData(
-    val title : String,
-    val date : Date
+    val title: String,
+    val date: Date
 )
