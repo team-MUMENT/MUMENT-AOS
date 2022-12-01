dd1ca5ba0ad9f5fa6043aca04a946b28b5646141
jinsilWoo <wryt9224@gmail.com>
2022-12-02 02:20:29 +0900

    [FEAT] #137- Move Notification Setting in the App

--- feature/mypage/src/main/java/com/mument_android/mypage/fragment/AlarmSettingFragment.kt
+++ feature/mypage/src/main/java/com/mument_android/mypage/fragment/AlarmSettingFragment.kt
@@ -1,11 +1,16 @@
 package com.mument_android.mypage.fragment
 
+import android.content.ActivityNotFoundException
+import android.content.Context
+import android.content.Intent
+import android.os.Build
 import android.os.Bundle
+import android.provider.Settings
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import androidx.fragment.app.FragmentManager
+import androidx.annotation.RequiresApi
 import androidx.fragment.app.commit
 import androidx.preference.PreferenceFragmentCompat
 import com.mument_android.core_dependent.util.AutoClearedValue
@@ -27,11 +32,55 @@ class AlarmSettingFragment : Fragment() {
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
-            childFragmentManager.commit {
-                add(R.id.fc_preference_fragment, SettingPreferenceFragment())
-            }
+        setPreferenceFragment()
+        openAppAlarmSetting(requireContext())
 
     }
+
+    //설정 프래그먼트 fragment container 에 세팅
+    private fun setPreferenceFragment() {
+        childFragmentManager.commit {
+            add(R.id.fc_preference_fragment, SettingPreferenceFragment())
+        }
+    }
+
+    //앱 내의 알림설정 화면 띄우기
+    private fun openAppAlarmSetting(context: Context) {
+        val intent = if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+            alarmSettingVersionUp(context)
+        } else {
+            alarmSettingVersionDown(context)
+        }
+        try {
+            context.startActivity(intent)
+        } catch (e: ActivityNotFoundException) {
+            e.printStackTrace()
+        }
+    }
+
+    //API 26 이상일 떄의  내 알림 설정 보여주기
+    @RequiresApi(Build.VERSION_CODES.O)
+    private fun alarmSettingVersionUp(context: Context): Intent {
+        return Intent().apply {
+            action = Settings.ACTION_APP_NOTIFICATION_SETTINGS
+            putExtra(Settings.EXTRA_APP_PACKAGE, context.packageName)
+            flags = Intent.FLAG_ACTIVITY_NEW_TASK
+        }
+    }
+
+    // API 26 이하 일 때
+    private fun alarmSettingVersionDown(context: Context): Intent {
+        return Intent().apply {
+            action = "android.settings.APP_NOTIFICATION_SETTINGS"
+            putExtra("app_package", context.packageName)
+            putExtra("app_uid",context.applicationInfo?.uid)
+            flags = Intent.FLAG_ACTIVITY_NEW_TASK
+        }
+    }
+
+
+
+
 
     class SettingPreferenceFragment : PreferenceFragmentCompat() {
         override fun onCreatePreferences(savedInstanceState: Bundle?, rootKey: String?) {
