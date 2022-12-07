2930efbf2f03ec3d2dd3aea3b307f75881effcc5
heaven00 <69586104+lhb8106@users.noreply.github.com>
2022-12-07 23:44:40 +0900

    [FEAT] #144 스플래시 구현 끝

---
+++ feature/login/src/main/java/com/mument_android/login/SplashActivity.kt
@@ -0,0 +1,31 @@
+package com.mument_android.login
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.os.Handler
+import android.os.Looper
+import com.mument_android.core_dependent.base.BaseActivity
+import com.mument_android.login.databinding.ActivitySplashBinding
+
+class SplashActivity : BaseActivity<ActivitySplashBinding>(R.layout.activity_splash) {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        initSplash()
+    }
+
+    //스플래시 -> 우선은 로그인으로 가는 로직 (후에 토큰 관리하다보면 login or main 분기처리)
+    private fun initSplash() {
+        Handler(Looper.getMainLooper()).postDelayed({
+            val intent = Intent(this, LogInActivity::class.java)
+            intent.addFlags(Intent.FLAG_ACTIVITY_NO_ANIMATION)
+            startActivity(intent)
+            finish()
+        },DURATION)
+    }
+
+    companion object {
+        private const val DURATION : Long = 2000
+    }
+
+}
