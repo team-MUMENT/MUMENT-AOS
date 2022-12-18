b198de485079682ae9bd7a5b1b0e4dfc4093306d
jinsilWoo <wryt9224@gmail.com>
2022-12-18 23:28:12 +0900

    [FEAT]#142- UnregisterFragment UI

---
+++ feature/mypage/src/main/java/com/mument_android/mypage/fragment/UnregisterFragment.kt
@@ -0,0 +1,44 @@
+package com.mument_android.mypage.fragment
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.viewModels
+import com.mument_android.core_dependent.util.AutoClearedValue
+import com.mument_android.mypage.MyPageViewModel
+import com.mument_android.mypage.R
+import com.mument_android.mypage.databinding.FragmentUnregisterBinding
+
+class UnregisterFragment : Fragment() {
+
+    private var binding by AutoClearedValue<FragmentUnregisterBinding>()
+    private val myPageViewModel: MyPageViewModel by viewModels()
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View = FragmentUnregisterBinding.inflate(inflater, container, false).run {
+        binding = this
+        this.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        binding.lifecycleOwner = viewLifecycleOwner
+        binding.myPageViewModel = myPageViewModel
+
+        radioBtnEvent()
+    }
+
+    private fun radioBtnEvent(){
+        binding.rgChooseReason.setOnCheckedChangeListener{_,checkedID->
+            when(checkedID){
+               R.id.unregister_reason_first->{
+
+                }
+            }
+        }
+    }
+}
