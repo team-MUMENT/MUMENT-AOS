2fe2b40e1ced30b7e4d18f2f22a2f086b6964610
minho <my_alsgh@naver.com>
2022-12-19 23:58:05 +0900

    [REFACTOR] Unused File Remove

--- data/src/main/java/com/mument_android/data/datasource/home/RemoteMumentHistoryDataSourceImpl.kt
+++ data/src/main/java/com/mument_android/data/datasource/home/RemoteMumentHistoryDataSourceImpl.kt
@@ -8,7 +8,6 @@ import javax.inject.Inject
 class RemoteMumentHistoryDataSourceImpl @Inject constructor(val homeService:HomeService):
     RemoteMumentHistoryDataSource {
     override suspend fun getMumentHistory(userId:String, musicId:String): BaseResponse<MumentHistoryDto> {
-        val data = homeService.getMumentHistory(userId, musicId)
-        return data
+        return homeService.getMumentHistory(userId, musicId)
     }
 }
