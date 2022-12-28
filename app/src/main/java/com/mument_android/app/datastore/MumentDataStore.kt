1cf0dc5a568ebc5929cc85e53bca9c286e2739bb
heaven00 <69586104+lhb8106@users.noreply.github.com>
2022-12-28 22:53:37 +0900

    [FEAT] #144 delete code

--- app/src/main/java/com/mument_android/app/datastore/MumentDataStore.kt
+++ app/src/main/java/com/mument_android/app/datastore/MumentDataStore.kt
@@ -17,8 +17,6 @@ import java.util.prefs.Preferences
 import javax.inject.Singleton
 
 
-private const val LAYOUT_PREFERENCES_NAME = "layout_preferences"
-
 @InstallIn(SingletonComponent::class)
 @Module
 object MumentDataStore {
