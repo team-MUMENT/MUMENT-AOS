cf9734b450f600bbdb18222938e69d45ba5037a0
heaven00 <69586104+lhb8106@users.noreply.github.com>
2022-12-28 23:51:48 +0900

    [FEAT] #143 add image and string

--- feature/login/build.gradle.kts
+++ feature/login/build.gradle.kts
@@ -44,6 +44,9 @@ android {
 
 dependencies {
     implementation(project(Modules.CORE_DEPENDENT_MODULE))
+    implementation("androidx.appcompat:appcompat:1.5.1")
+    implementation("com.google.android.material:material:1.4.0")
+    implementation("androidx.constraintlayout:constraintlayout:2.1.4")
     addAndroidXDependencies()
     addTestDependencies()
     addNavigationDependencies()
---
+++ feature/login/src/main/java/com/mument_android/login/ProfileSettingActivity.kt
@@ -0,0 +1,11 @@
+package com.mument_android.login
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class ProfileSettingActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_profile_setting)
+    }
+}
