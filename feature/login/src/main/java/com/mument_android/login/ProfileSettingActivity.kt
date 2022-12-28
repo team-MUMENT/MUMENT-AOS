68927642a81a9d85ffa5e77368e701ac4f359f06
heaven00 <69586104+lhb8106@users.noreply.github.com>
2022-12-29 00:43:20 +0900

    [FEAT] #143 profile setting view end

--- feature/login/src/main/java/com/mument_android/login/ProfileSettingActivity.kt
+++ feature/login/src/main/java/com/mument_android/login/ProfileSettingActivity.kt
@@ -1,11 +1,14 @@
 package com.mument_android.login
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.mument_android.core_dependent.base.BaseActivity
+import com.mument_android.login.databinding.ActivityProfileSettingBinding
 
-class ProfileSettingActivity : AppCompatActivity() {
+class ProfileSettingActivity :  BaseActivity<ActivityProfileSettingBinding>(R.layout.activity_profile_setting) {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_profile_setting)
     }
+
+
+
 }
