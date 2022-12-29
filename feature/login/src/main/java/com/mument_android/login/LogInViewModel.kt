d4cbea4bc7e267f5679cbd06915077984fd97c4c
heaven00 <69586104+lhb8106@users.noreply.github.com>
2022-12-30 02:29:36 +0900

    [FEAT] #143 trim 함수 활용

--- feature/login/src/main/java/com/mument_android/login/LogInViewModel.kt
+++ feature/login/src/main/java/com/mument_android/login/LogInViewModel.kt
@@ -1,9 +1,15 @@
 package com.mument_android.login
 
+import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 import dagger.hilt.android.lifecycle.HiltViewModel
 import javax.inject.Inject
 
 @HiltViewModel
 class LogInViewModel @Inject constructor(): ViewModel() {
+
+    val mumentNickName = MutableLiveData<String>()
+    val isRightPattern = MutableLiveData<Boolean>()
+    val isActive = MutableLiveData<Boolean>()
+
 }
--- feature/login/src/main/java/com/mument_android/login/ProfileSettingActivity.kt
+++ feature/login/src/main/java/com/mument_android/login/ProfileSettingActivity.kt
@@ -1,18 +1,56 @@
 package com.mument_android.login
 
 import android.os.Bundle
+import android.system.Os.remove
+import android.text.Editable
+import android.text.TextWatcher
+import android.util.Log
 import androidx.activity.viewModels
 import com.mument_android.core_dependent.base.BaseActivity
 import com.mument_android.login.databinding.ActivityProfileSettingBinding
+import java.util.regex.Pattern
 
 class ProfileSettingActivity :  BaseActivity<ActivityProfileSettingBinding>(R.layout.activity_profile_setting) {
 
     private val viewModel: LogInViewModel by viewModels()
+    private val finalNickname = ""
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding.viewModel = viewModel
         binding.lifecycleOwner = this
+
+        isRightPattern()
+        isSpace()
+    }
+
+    private fun isRightPattern() {
+        val nickname = viewModel.mumentNickName
+        if(!Pattern.matches("^[ㄱ-ㅎ가-힣a-zA-Z0-9]{2,15}\$", nickname.toString())) {
+            viewModel.isRightPattern.value = false
+            binding.tvPattern.isSelected = true
+        } else {
+            viewModel.isRightPattern.value = true
+            binding.tvPattern.isSelected = false
+        }
+    }
+
+    private fun isSpace() {
+        binding.etNickname.addTextChangedListener(object : TextWatcher{
+            override fun beforeTextChanged(p0: CharSequence?, p1: Int, p2: Int, p3: Int) {
+
+            }
+
+            override fun onTextChanged(p0: CharSequence?, p1: Int, p2: Int, p3: Int) {
+
+            }
+
+            override fun afterTextChanged(p0: Editable?) {
+                val str = p0?.trim().toString()
+                viewModel.isActive.value = str.length >= 2
+            }
+
+        })
     }
 
 
