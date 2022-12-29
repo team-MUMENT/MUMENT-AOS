b3cee9664cbe08ba632f0bba9882e9fdd3cae3ea
heaven00 <69586104+lhb8106@users.noreply.github.com>
2022-12-30 00:56:55 +0900

    [FEAT] #143 profile add viewModel

---
+++ feature/login/src/main/java/com/mument_android/login/LogInViewModel.kt
@@ -0,0 +1,9 @@
+package com.mument_android.login
+
+import androidx.lifecycle.ViewModel
+import dagger.hilt.android.lifecycle.HiltViewModel
+import javax.inject.Inject
+
+@HiltViewModel
+class LogInViewModel @Inject constructor(): ViewModel() {
+}
--- feature/login/src/main/java/com/mument_android/login/ProfileSettingActivity.kt
+++ feature/login/src/main/java/com/mument_android/login/ProfileSettingActivity.kt
@@ -1,12 +1,18 @@
 package com.mument_android.login
 
 import android.os.Bundle
+import androidx.activity.viewModels
 import com.mument_android.core_dependent.base.BaseActivity
 import com.mument_android.login.databinding.ActivityProfileSettingBinding
 
 class ProfileSettingActivity :  BaseActivity<ActivityProfileSettingBinding>(R.layout.activity_profile_setting) {
+
+    private val viewModel: LogInViewModel by viewModels()
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        binding.viewModel = viewModel
+        binding.lifecycleOwner = this
     }
 
 
