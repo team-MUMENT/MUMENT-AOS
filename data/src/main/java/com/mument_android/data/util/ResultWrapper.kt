70dd981dbcfac13f0ff93f8e5d4c77b90ac9564c
minho <my_alsgh@naver.com>
2022-12-30 16:03:05 +0900

    [Chore] ResultWrapper Local Error Add

--- data/src/main/java/com/mument_android/data/util/ResultWrapper.kt
+++ data/src/main/java/com/mument_android/data/util/ResultWrapper.kt
@@ -3,5 +3,6 @@ package com.mument_android.data.util
 sealed class ResultWrapper<out T> {
     data class Success<out T>(val data: T) : ResultWrapper<T>()
     data class GenericError(val code: Int?, val message: String?) : ResultWrapper<Nothing>()
+    data class LocalError(val message: String?) : ResultWrapper<Nothing>()
     object NetworkError : ResultWrapper<Nothing>()
 }
