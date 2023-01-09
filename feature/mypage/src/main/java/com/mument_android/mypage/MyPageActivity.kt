478268fae3955b588508ec9ad0468e9bb4bff8dd
jinsilWoo <wryt9224@gmail.com>
2023-01-09 22:24:00 +0900

    [FEAT]#155- move to web view

--- feature/mypage/src/main/java/com/mument_android/mypage/MyPageActivity.kt
+++ feature/mypage/src/main/java/com/mument_android/mypage/MyPageActivity.kt
@@ -4,11 +4,13 @@ import android.content.Intent
 import android.os.Bundle
 import android.view.View
 import androidx.activity.viewModels
+import androidx.core.content.ContextCompat
 import androidx.core.os.bundleOf
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.commit
 import androidx.fragment.app.commitNow
 import com.mument_android.core_dependent.base.BaseActivity
+import com.mument_android.core_dependent.base.WebViewActivity
 import com.mument_android.mypage.databinding.ActivityMyPageBinding
 import com.mument_android.mypage.fragment.*
 
@@ -22,6 +24,7 @@ class MyPageActivity : BaseActivity<ActivityMyPageBinding>(R.layout.activity_my_
         binding.myPageViewModel = myPageViewModel
 
         transactionBtnEvent()
+        clickListenerWebView()
         moveUnregister()
     }
 
@@ -45,6 +48,35 @@ class MyPageActivity : BaseActivity<ActivityMyPageBinding>(R.layout.activity_my_
         }
     }
 
+    //각 웹뷰로 이동
+    private fun clickListenerWebView(){
+        with(binding){
+            //자주묻는질문
+            btnMyPageGoFAQ.setOnClickListener {
+                initIntent("https://www.naver.com/")
+            }
+            //문의하기
+            btnMyPageGoInquiry.setOnClickListener{
+                initIntent("https://www.naver.com/")
+            }
+            //앱정보
+            btnMyPageGoAppInfo.setOnClickListener{
+                initIntent("https://www.naver.com/")
+            }
+            //뮤멘트 소개
+            btnMyPageGoIntroduceMument.setOnClickListener{
+                initIntent("https://www.naver.com/")
+            }
+        }
+    }
+
+    private fun initIntent(url: String) {
+        val intent = Intent(this, WebViewActivity::class.java)
+        intent.putExtra("url", url)
+        ContextCompat.startActivity(this, intent, null)
+    }
+
+    //회원탈퇴뷰 이동
     private fun moveUnregister() {
         binding.tvMyPageUnregister.setOnClickListener {
             supportFragmentManager.commit() {
