181c2dff22e07a4a91d84da6c9c7fe527c152cdd
minho <my_alsgh@naver.com>
2023-01-10 21:04:30 +0900

    [FEAT] Contract Add

---
+++ feature/home/src/main/java/com/mument_android/home/notify/NotifyContract.kt
@@ -0,0 +1,27 @@
+package com.mument_android.home.notify
+
+import com.mument_android.core.util.Event
+import com.mument_android.core.util.SideEffect
+import com.mument_android.core.util.ViewState
+import com.mument_android.home.models.Notify
+
+class NotifyContract {
+
+    data class NotifyViewState(
+        override val hasError: Boolean = false,
+        override val onNetwork: Boolean = false,
+        val notifyList: List<Notify>? = null
+    ) : ViewState
+
+    sealed class NotifyEvent : Event {
+        object OnClickBackBtn : NotifyEvent()
+        object OnClickSettingBtn : NotifyEvent()
+        data class OnClickNotify(val notify: Notify) : NotifyEvent()
+    }
+
+    sealed class NotifySideEffect : SideEffect {
+        object PopBackStack : NotifySideEffect()
+        data class Toast(val message: String) : NotifySideEffect()
+        data class NavToMumentDetail(val mumentId: String) : NotifySideEffect()
+    }
+}
--- feature/home/src/main/java/com/mument_android/home/viewmodels/NotifyViewModel.kt
+++ feature/home/src/main/java/com/mument_android/home/viewmodels/NotifyViewModel.kt
@@ -1,6 +1,15 @@
 package com.mument_android.home.viewmodels
 
 import androidx.lifecycle.ViewModel
+import com.mument_android.home.notify.NotifyContract.*
+import kotlinx.coroutines.channels.Channel
+import kotlinx.coroutines.flow.MutableSharedFlow
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.receiveAsFlow
 
 class NotifyViewModel:ViewModel() {
+    private val _notifyEvent: MutableSharedFlow<NotifyEvent> = MutableSharedFlow()
+    private val _notifyEffect: Channel<NotifySideEffect> = Channel()
+    val notifyEffect get() = _notifyEffect.receiveAsFlow()
+
 }
