fdcd88094be08a63706227de7d5b57bee95164f4
minho <my_alsgh@naver.com>
2023-01-12 19:29:31 +0900

    [FEAT] Notify Data Model Setting

--- feature/home/src/main/java/com/mument_android/home/models/Notify.kt
+++ feature/home/src/main/java/com/mument_android/home/models/Notify.kt
@@ -1,6 +1,12 @@
 package com.mument_android.home.models
 
-import com.mument_android.domain.entity.musicdetail.musicdetaildata.Music
 import com.mument_android.home.util.NotifyType
 
-data class Notify(val title:String, val music: Music? = null, val notifyType: NotifyType)
+data class Notify(
+    val id: String,
+    val createdAt: String,
+    val image: String,
+    val newsId: Int,
+    val title: String,
+    val notifyType: NotifyType
+)
