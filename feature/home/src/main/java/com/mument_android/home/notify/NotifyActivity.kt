4d2a5b0a6903afdffd5a3067a969647089788389
minho <my_alsgh@naver.com>
2023-01-13 01:17:11 +0900

    [CHORE] Dummy List Add, Anim Test

--- feature/home/src/main/java/com/mument_android/home/notify/NotifyActivity.kt
+++ feature/home/src/main/java/com/mument_android/home/notify/NotifyActivity.kt
@@ -1,5 +1,6 @@
 package com.mument_android.home.notify
 
+import android.content.Intent
 import android.os.Bundle
 import androidx.activity.viewModels
 import com.mument_android.core_dependent.base.BaseActivity
@@ -11,6 +12,8 @@ import com.mument_android.home.adapters.NotifyAdapter
 import com.mument_android.home.databinding.ActivityNotifyBinding
 import com.mument_android.home.models.Notify
 import com.mument_android.home.notify.NotifyContract.*
+import com.mument_android.home.search.SearchActivity
+import com.mument_android.home.util.NotifyType
 import com.mument_android.home.viewmodels.NotifyViewModel
 import dagger.hilt.android.AndroidEntryPoint
 
@@ -67,6 +70,8 @@ class NotifyActivity : BaseActivity<ActivityNotifyBinding>(
 
     private fun moveToMumentDetail(notify: Notify) {
         //Move MumentDetail
+        startActivity(Intent(this, SearchActivity::class.java))
+        finish()
     }
 
     private fun moveToNoticeView(notify: Notify) {
@@ -74,11 +79,227 @@ class NotifyActivity : BaseActivity<ActivityNotifyBinding>(
     }
 
     private fun adapterSetting() {
-        notifyAdapter = NotifyAdapter { notifyViewModel.emitEvent(NotifyEvent.OnClickNotify(it)) }
+        notifyAdapter = NotifyAdapter({ notifyViewModel.emitEvent(NotifyEvent.OnClickNotify(it)) },
+            { notifyViewModel.emitEvent(NotifyEvent.OnDeleteNotify(it)) })
         binding.rvNotifyList.adapter = notifyAdapter
         notifyAdapter.submitList(
             listOf(
-
+                Notify(
+                    id = "",
+                    createdAt = "02/05 09:10",
+                    "",
+                    1,
+                    "예진님이 POPPOPOPOPOPOPOPOPOPOPOPOPOPOPOPOPOPOPOPOPOPOPOPOPOPOPOPOPOPOPOPOPOPOPOPOPOPOOPOPOPOPOPOPOPOPOPOPOPOPOPOPOPOPO에 쓴 뮤멘트를 좋아합니다.", //단어가 길면 자동 줄바꿈이 일어나므로 유니코드 변경, 나중에 전반적으로 수정할 예정,
+                    NotifyType.LIKE
+                ),
+                Notify(
+                    id = "",
+                    createdAt = "02/05 09:10",
+                    "",
+                    1,
+                    "예진님이 POPPOPOPOPOPOPOPOPOPOPOPOPOPOPOPOPOPOPOPOPOPOPOPOPOPOPOPOPOPOPOPOPOPOPOPOPOPOPOPOPOPO에 쓴 뮤멘트를 좋아합니다.",
+                    NotifyType.LIKE
+                ),
+                Notify(
+                    id = "",
+                    createdAt = "02/05 09:10",
+                    "",
+                    1,
+                    "예진님이 POPPOPOPOPOPOPOPOPOPOPOPOPOPOPOPOPOPOPOPOPOPOPOPOPOPOPOPOPOPOPOPOPOPOPOPOPOPOPOPOPOPO에 쓴 뮤멘트를 좋아합니다.",
+                    NotifyType.LIKE
+                ),
+                Notify(
+                    id = "",
+                    createdAt = "02/05 09:10",
+                    "",
+                    1,
+                    "예진님이 POPPOPOPOPOPOPOPOPOPOPOPOPOPOPOPOPOPOPOPOPOPOPOPOPOPOPOPOPOPOPOPOPOPOPOPOPOPOPOPOPOPO에 쓴 뮤멘트를 좋아합니다.",
+                    NotifyType.LIKE
+                ),
+                Notify(
+                    id = "",
+                    createdAt = "02/05 09:10",
+                    "",
+                    1,
+                    "예진님이 POPPOPOPOPOPOPOPOPOPOPOPOPOPOPOPOPOPOPOPOPOPOPOPOPOPOPOPOPOPOPOPOPOPOPOPOPOPOPOPOPOPO에 쓴 뮤멘트를 좋아합니다.",
+                    NotifyType.LIKE
+                ),
+                Notify(
+                    id = "",
+                    createdAt = "02/05 09:10",
+                    "",
+                    1,
+                    "예진님이 POPPOPOPOPOPOPOPOPOPOPOPOPOPOPOPOPOPOPOPOPOPOPOPOPOPOPOPOPOPOPOPOPOPOPOPOPOPOPOPOPOPO에 쓴 뮤멘트를 좋아합니다.",
+                    NotifyType.LIKE
+                ),
+                Notify(
+                    id = "",
+                    createdAt = "02/05 09:10",
+                    "",
+                    1,
+                    "예진님이 POPPOPOPOPOPOPOPOPOPOPOPOPOPOPOPOPOPOPOPOPOPOPOPOPOPOPOPOPOPOPOPOPOPOPOPOPOPOPOPOPOPO에 쓴 뮤멘트를 좋아합니다.",
+                    NotifyType.LIKE
+                ),
+                Notify(
+                    id = "",
+                    createdAt = "02/05 09:10",
+                    "",
+                    1,
+                    "예진님이 POPPOPOPOPOPOPOPOPOPOPOPOPOPOPOPOPOPOPOPOPOPOPOPOPOPOPOPOPOPOPOPOPOPOPOPOPOPOPOPOPOPO에 쓴 뮤멘트를 좋아합니다.",
+                    NotifyType.LIKE
+                ),
+                Notify(
+                    id = "",
+                    createdAt = "02/05 09:10",
+                    "",
+                    1,
+                    "예진님이 흔들리는 꽃들 속에서 내 샴푸향이 느껴진거야~~~흔들리는 꽃들 속에서 내 샴푸향이 느껴진거야~~~흔들리는 꽃들 속에서 내 샴푸향이 느껴진거야~~~흔들리는 꽃들 속에서 내 샴푸향이 느껴진거야~~~OPOPOPOPOPOPO에 쓴 뮤멘트를 좋아합니다.",
+                    NotifyType.LIKE
+                ),
+                Notify(
+                    id = "",
+                    createdAt = "02/05 09:10",
+                    "",
+                    1,
+                    "예진님이 POPPOPOPOPOPOPOPOPOPOPOPOPOPOPOPO흔들리는 꽃들 속에서 내 샴푸향이 느껴진거야~~~흔들리는 꽃들 속에서 내 샴푸향이 느껴진거야~~~OPOPOPOPOPOPOPO에 쓴 뮤멘트를 좋아합니다.",
+                    NotifyType.LIKE
+                ),
+                Notify(
+                    id = "",
+                    createdAt = "02/05 09:10",
+                    "",
+                    1,
+                    "예진님이 POPPOPOPOPOPOPOPOPOPOPOPOPOPOPOPOPOPOPOPOPOPOPO흔들리는 꽃들 속에서 내 샴푸향이 느껴진거야~~~흔들리는 꽃들 속에서 내 샴푸향이 느껴진거야~~~POPOPOPOPOPOPOPOPOPO에 쓴 뮤멘트를 좋아합니다.",
+                    NotifyType.LIKE
+                ),
+                Notify(
+                    id = "",
+                    createdAt = "02/05 09:10",
+                    "",
+                    1,
+                    "예진님이 POPPOPOPOPOPOPOPOPOPOPOPOPOPOPOPOPOPOPOPOPOPOPOPOPO흔들리는 꽃들 속에서 내 샴푸향이 느껴진거야~~~POPOPOPOPOPOPOPOPOPOPOPOPO에 쓴 뮤멘트를 좋아합니다.",
+                    NotifyType.LIKE
+                ),
+                Notify(
+                    id = "",
+                    createdAt = "02/05 09:10",
+                    "",
+                    1,
+                    "예진님이 POPPOPOPOPOPOPOPOPOPOPOPOPOPOPOPOPOPOPOPOPOPOPOPOPOPOPOPOPOPOPOPOPOPOPOPOPOPOPOPOPOPO에 쓴 뮤멘트를 좋아합니다.",
+                    NotifyType.LIKE
+                ),
+                Notify(
+                    id = "",
+                    createdAt = "02/05 09:10",
+                    "",
+                    1,
+                    "예진님이 POPPOPOPOPOPOPOPOPOPOPOPOPOPOPOPOPOPOPOPOPOPOPOPOPOPOPOP흔들리는 꽃들 속에서 내 샴푸향이 느껴진거야~~~POPOPOPOPOPOPOPOPOPOPO에 쓴 뮤멘트를 좋아합니다.",
+                    NotifyType.LIKE
+                ),
+                Notify(
+                    id = "",
+                    createdAt = "02/05 09:10",
+                    "",
+                    1,
+                    "예진님이 POPPOPOPOPOPOPOPOPOPOPOPOPOPOPOPOPOPOPOPOPOPOP흔들리는 꽃들 속에서 내 샴푸향이 느껴진거야~~~OPOPOPOPOPOPOPOPOPOPO에 쓴 뮤멘트를 좋아합니다.",
+                    NotifyType.LIKE
+                ),
+                Notify(
+                    id = "",
+                    createdAt = "02/05 09:10",
+                    "",
+                    1,
+                    "예진님이 POPPOPOPOPOPOPOPOPOPOPOPOPOPOPOPOPOPOPOPOPOPOPOPOPOPOPOPOPOPOPOPOPOPOPOPOPOPOPOPOPOPO에 쓴 뮤멘트를 좋아합니다.",
+                    NotifyType.LIKE
+                ),
+                Notify(
+                    id = "",
+                    createdAt = "02/05 09:10",
+                    "",
+                    1,
+                    "예진님이 POPPOPOPOPOPOPOPOPOPOPOPOPOPOPOPOPOPOPOPOPOPOPOPO흔들리는 꽃들 속에서 내 샴푸향이 느껴진거야~~~POPOPOPOPOPOPOPOPOPOPOPO에 쓴 뮤멘트를 좋아합니다.",
+                    NotifyType.LIKE
+                ),
+                Notify(
+                    id = "",
+                    createdAt = "02/05 09:10",
+                    "",
+                    1,
+                    "예진님이 POPPOPOPOPOPOPOPOPOPOPOPOPOPOPOPOPOPOPOPO흔들리는 꽃들 속에서 내 샴푸향이 느껴진거야~~~POPOPOPOPOPOPOPOPOPOPOPO에 쓴 뮤멘트를 좋아합니다.",
+                    NotifyType.LIKE
+                ),
+                Notify(
+                    id = "",
+                    createdAt = "02/05 09:10",
+                    "",
+                    1,
+                    "예진님이 POPPOPOPOPOPOPOPOPOPOPOPOPOPOPOPOPOPOPOPOPOPOPOPOPOPOPOPOPOPOPOPOPOPOPOPOPOPOPOPOPOPO에 쓴 뮤멘트를 좋아합니다.",
+                    NotifyType.LIKE
+                ),
+                Notify(
+                    id = "",
+                    createdAt = "02/05 09:10",
+                    "",
+                    1,
+                    "예진님이 POPPOPOPOPOPOPOPOPOPOPOPOPOPOPOPOPOPOPOPOPOPOPOPOPOPOPOPOPOPOPOPOPOPOPOPOPOPOPOOPOPOPOPOPOPOPOPOPOPOPOPOPOOPOPOPOPOPOPOPOPOPOPOPOPOPOPOEnd에 쓴 뮤멘트를 좋아합니다.",
+                    NotifyType.LIKE
+                ),
+                Notify(
+                    id = "",
+                    createdAt = "02/05 09:10",
+                    "",
+                    1,
+                    "예진님이 POPPOPOPOPOPOPOPOPOPOPOPOPOPOPOPOPOPOPOPOPOPOPOPOPOPOPOPOPOPOPOPOPOPOPOPOPOPOOPOPOPOPOPOPOPOPOPOPOPOPOPOPOPOPOEnd에 쓴 뮤멘트를 좋아합니다.",
+                    NotifyType.LIKE
+                ),
+                Notify(
+                    id = "",
+                    createdAt = "02/05 09:10",
+                    "",
+                    1,
+                    "예진님이 POPPOPOPOPOPOPOPOPOPOPOPOPOPOPOPOPOPOPOPOPOPOPOPOPOPOPOPOPOPOPOPOPOPOPOPOPOOPOPOPOPOPOPOPOPOPOPOPOPOPOPOPOPOPOEnd에 쓴 뮤멘트를 좋아합니다.",
+                    NotifyType.LIKE
+                ),
+                Notify(
+                    id = "",
+                    createdAt = "02/05 09:10",
+                    "",
+                    1,
+                    "예진님이 POPPOPOPOPOPOPOPOPOPOPOPOPOPOPOPOPOPOPOPOPOPOPOPOPOPOPOPOPOPOPOPOPOPOPOPOPOOPOPOPOPOPOPOPOPOPOPOPOPOPOPOPOPOPOEnd에 쓴 뮤멘트를 좋아합니다.",
+                    NotifyType.LIKE
+                ),
+                Notify(
+                    id = "",
+                    createdAt = "02/05 09:10",
+                    "",
+                    1,
+                    "예진님이 POPPOPOPOPOPOPOPOPOPOPO에 쓴 뮤멘트를 좋아합니다.",
+                    NotifyType.LIKE
+                ),
+                Notify(
+                    id = "",
+                    createdAt = "02/05 09:10",
+                    "",
+                    1,
+                    "예진님이 POPPOPOPOPOPOPOPOPOPOPOPOPOPOPOPOPOPOPOPOPOPOPOPOPOPOPOPOPOPOPOPOPOPOPOPOPOPOPOPOPOPOEnd에 쓴 뮤멘트를 좋아합니다.",
+                    NotifyType.LIKE
+                ),
+                Notify(
+                    id = "",
+                    createdAt = "02/05 09:10",
+                    "",
+                    1,
+                    "예진님이 POPPOPOPOPOPOPOPOPOPOPOPOPOPOPOPOPOPOPOPOPOPOPOPOPOPOPOPOPOPOPOPOPOPOPOPOPOPOPOPOPOPOEnd에 쓴 뮤멘트를 좋아합니다.",
+                    NotifyType.LIKE
+                ),
+                Notify(
+                    id = "",
+                    createdAt = "02/05 09:10",
+                    "",
+                    1,
+                    "예진님이 POPPOPOPOPOPOPOPOPOPOPOPO에 쓴 뮤멘트를 좋아합니다.",
+                    NotifyType.LIKE
+                ),
             )
         )
     }
