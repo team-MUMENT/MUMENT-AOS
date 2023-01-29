81e2a1d495d4499e883969ef52fab60cfbc277b8
minho <my_alsgh@naver.com>
2023-01-30 01:28:05 +0900

    [FEAT] Apply API Document Modification

--- feature/home/src/main/java/com/mument_android/home/notify/NotifyEllipsizeCustomTextView.kt
+++ feature/home/src/main/java/com/mument_android/home/notify/NotifyEllipsizeCustomTextView.kt
@@ -6,6 +6,7 @@ import android.text.SpannableStringBuilder
 import android.util.AttributeSet
 import androidx.appcompat.widget.AppCompatTextView
 import com.mument_android.home.R
+import com.mument_android.home.util.NotifyType
 import com.mument_android.home.util.TextStyleCustomHelper
 
 class NotifyEllipsizeCustomTextView @JvmOverloads constructor(
@@ -13,9 +14,12 @@ class NotifyEllipsizeCustomTextView @JvmOverloads constructor(
 ) : AppCompatTextView(context, attrs) {
     private lateinit var textStyleCustomHelper: TextStyleCustomHelper
     private var isApplySpanLimit = 0
-    fun clearSpanCount(){
+    lateinit var notiType: NotifyType
+
+    fun clearSpanCount() {
         isApplySpanLimit = 0
     }
+
     override fun draw(canvas: Canvas?) {
         if (text != null) {
             textStyleCustomHelper = TextStyleCustomHelper(text)
@@ -23,15 +27,24 @@ class NotifyEllipsizeCustomTextView @JvmOverloads constructor(
                 textStyleCustomHelper.setEllipsizeMusic(layout = layout) ?: SpannableStringBuilder(
                     text
                 )
-            if (isApplySpanLimit < 2) {   //text = builder 이후 한 번 더 불려야함
-                if (textStyleCustomHelper.applyNotifySpan(text = builder, nameColor = context.getColor(R.color.mument_color_blue1))) {
+            //text = builder 이후 한 번 더 불려야함
+            if (isApplySpanLimit < 2) {
+                if (::notiType.isInitialized && notiType == NotifyType.LIKE && textStyleCustomHelper.applyNotifySpan(
+                        text = builder,
+                        nameColor = context.getColor(R.color.mument_color_blue1)
+                    )
+                ) {
                     text = builder
-                    isApplySpanLimit++
                 }
+                isApplySpanLimit++
             }
         } else {
             isApplySpanLimit = 0
         }
         super.draw(canvas)
+    }
+
+    fun setTypeOfNotify(type: NotifyType) {
+        notiType = type
     }
 }
