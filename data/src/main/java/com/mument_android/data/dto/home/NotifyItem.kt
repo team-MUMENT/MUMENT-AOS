633a48a8660d803049fbf6382094f12e0e76cdcc
minho <my_alsgh@naver.com>
2023-01-30 19:17:31 +0900

    [FEAT] Notify Repository Add

--- data/src/main/java/com/mument_android/data/dto/home/NotifyItem.kt
+++ data/src/main/java/com/mument_android/data/dto/home/NotifyItem.kt
@@ -1,15 +1,15 @@
 package com.mument_android.data.dto.home
 
 data class NotifyItem(
-    val id: String,
+    val id: Int,
     val type: String,
     val userId: Int,
     val isDeleted: Boolean,
     val isRead: Boolean,
     val createdAt: String,
     val linkId: Int,
-    val noticePoint: String,
-    val noticeTitle: String,
-    val likeMusicTitle: String,
-    val likeProfileId: String,
+    val noticePoint: String?,
+    val noticeTitle: String?,
+    val likeMusicTitle: String?,
+    val likeProfileId: String?,
 )
---
+++ data/src/main/java/com/mument_android/data/repository/NotifyRepositoryImpl.kt
@@ -0,0 +1,55 @@
+package com.mument_android.data.repository
+
+import android.util.Log
+import com.mument_android.core.network.ApiStatus
+import com.mument_android.data.datasource.notify.NotifyDataSource
+import com.mument_android.data.mapper.notify.NotifyMapper
+import com.mument_android.data.util.ResultWrapper
+import com.mument_android.data.util.callApi
+import com.mument_android.domain.entity.home.NotifyEntity
+import com.mument_android.domain.repository.notify.NotifyRepository
+import javax.inject.Inject
+
+class NotifyRepositoryImpl @Inject constructor(
+    private val notifyDataSource: NotifyDataSource,
+    private val notifyMapper: NotifyMapper
+) : NotifyRepository {
+    override suspend fun fetchNotifyList(): ApiStatus<List<NotifyEntity>?> =
+        callApi(notifyMapper) {
+            notifyDataSource.fetchNotifyList()
+        }
+
+
+    override suspend fun fetchNotifyRead(unreadNews: List<Int>): Boolean? =
+        notifyDataSource.fetchNotifyRead(unreadNews).run {
+            when (this) {
+                is ResultWrapper.Success -> {
+                    true
+                }
+                is ResultWrapper.GenericError -> {
+                    Log.e("GenericeError", this.message!!)
+                    if (this.code!! == 400) false else null
+                }
+                else -> {
+                    null
+                }
+            }
+        }
+
+
+    override suspend fun fetchNotifyDelete(newsId: String): Boolean? =
+        notifyDataSource.fetchNotifyDelete(newsId).run {
+            when (this) {
+                is ResultWrapper.Success -> {
+                    true
+                }
+                is ResultWrapper.GenericError -> {
+                    Log.e("GenericeError", this.message!!)
+                    if (this.code!! == 400) false else null
+                }
+                else -> {
+                    null
+                }
+            }
+        }
+}
