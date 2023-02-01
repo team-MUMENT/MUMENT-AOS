38003f707c2fbc2103dc4fb1b5c93cd9bb10314b
jinsilWoo <wryt9224@gmail.com>
2023-02-02 04:23:48 +0900

    [FEAT]#171- block user profile image load

--- feature/mypage/src/main/java/com/mument_android/mypage/adapters/BlockUserManagementAdapter.kt
+++ feature/mypage/src/main/java/com/mument_android/mypage/adapters/BlockUserManagementAdapter.kt
@@ -4,8 +4,11 @@ import android.view.LayoutInflater
 import android.view.ViewGroup
 import androidx.recyclerview.widget.ListAdapter
 import androidx.recyclerview.widget.RecyclerView
+import coil.load
+import coil.transform.CircleCropTransformation
 import com.mument_android.core_dependent.util.GlobalDiffCallBack
 import com.mument_android.domain.entity.mypage.BlockUserEntity
+import com.mument_android.mypage.R
 import com.mument_android.mypage.databinding.ItemBlockUserBinding
 
 class BlockUserManagementAdapter(
@@ -17,6 +20,12 @@ class BlockUserManagementAdapter(
         RecyclerView.ViewHolder(binding.root) {
         fun bind(blockUserData: BlockUserEntity) {
             binding.tvUserName.text = blockUserData.profileId
+            binding.ivBlockUser.load(blockUserData.image) {
+                crossfade(true)
+                placeholder(R.drawable.mument_profile_sad_60_1)
+                transformations(CircleCropTransformation())
+            }
+
         }
     }
 
