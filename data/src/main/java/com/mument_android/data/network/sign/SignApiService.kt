c3a43835e12ca88cabf724bb2d031fe37a9df75c
heaven00 <69586104+lhb8106@users.noreply.github.com>
2023-02-01 23:33:44 +0900

    [FEAT] #173 - 불필요한 코드 삭제

--- data/src/main/java/com/mument_android/data/network/sign/SignApiService.kt
+++ data/src/main/java/com/mument_android/data/network/sign/SignApiService.kt
@@ -8,13 +8,11 @@ import retrofit2.Response
 import retrofit2.http.*
 
 interface SignApiService {
-    @Headers("Authorization:Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6MzAsInByb2ZpbGVJZCI6IuyViOuTnO2FjOyKpO2KuOyaqSIsImltYWdlIjpudWxsLCJpYXQiOjE2NzMxMjYzNzgsImV4cCI6MTY3NTcxODM3OCwiaXNzIjoiTXVtZW50In0.PG_Cubw4nv9USBiKKMVaAxS-Ggl6ByqOKusmyK4tp18")
     @GET("/user/profile/check/{profileId}")
     suspend fun signDuplicationCheck(
         @Path("profileId") profileId: String
     ): Response<Any?>
 
-    @Headers("Authorization:Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6MzAsInByb2ZpbGVJZCI6IuyViOuTnO2FjOyKpO2KuOyaqSIsImltYWdlIjpudWxsLCJpYXQiOjE2NzMxMjYzNzgsImV4cCI6MTY3NTcxODM3OCwiaXNzIjoiTXVtZW50In0.PG_Cubw4nv9USBiKKMVaAxS-Ggl6ByqOKusmyK4tp18")
     @Multipart
     @PUT("/user/profile")
     suspend fun putProfile(
