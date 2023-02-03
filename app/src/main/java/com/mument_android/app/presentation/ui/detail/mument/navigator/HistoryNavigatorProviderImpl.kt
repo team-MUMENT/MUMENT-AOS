00a04bf58aef4741550c7a5d0451245bfadacbc0
minho <my_alsgh@naver.com>
2023-02-03 22:45:16 +0900

    [FEAT] Move to History Params Modify

--- app/src/main/java/com/mument_android/app/presentation/ui/detail/mument/navigator/HistoryNavigatorProviderImpl.kt
+++ app/src/main/java/com/mument_android/app/presentation/ui/detail/mument/navigator/HistoryNavigatorProviderImpl.kt
@@ -1,20 +1,30 @@
 package com.mument_android.app.presentation.ui.detail.mument.navigator
 
 import android.app.Activity
+import android.content.Intent
 import android.os.Bundle
 import com.angdroid.navigation.HistoryNavigatorProvider
 import com.mument_android.R
 import com.mument_android.app.presentation.ui.main.MainActivity
+import com.mument_android.detail.history.HistoryActivity
+import com.mument_android.domain.entity.musicdetail.musicdetaildata.Music
 import com.mument_android.home.main.HomeFragment
 import javax.inject.Inject
 
-class HistoryNavigatorProviderImpl @Inject constructor(private val activity : Activity):
+class HistoryNavigatorProviderImpl @Inject constructor(private val activity: Activity) :
     HistoryNavigatorProvider {
-    override fun moveHistory(musicId: String) {
+    override fun moveHistory(music: Music, userId:Int) {
         with(activity as MainActivity) {
-            binding.navBar.selectedItemId = R.id.fragment_home
-            val bundle = Bundle().also { it.putString(HomeFragment.MUSIC_ID, musicId) }
-            this.navController.navigate(R.id.action_homeFragment_to_historyFragment, bundle)
+            when (binding.navBar.selectedItemId) {
+                R.id.fragment_home -> {}
+                else -> {
+                    binding.navBar.selectedItemId = R.id.fragment_home
+                }
+            }
+            startActivity(Intent(this, HistoryActivity::class.java).apply {
+                putExtra("music", music)
+                putExtra("userId", userId)
+            })
         }
     }
 }
