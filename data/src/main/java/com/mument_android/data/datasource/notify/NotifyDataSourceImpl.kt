1be2a91dc7292525f783d6857acd615d3d18b111
minho <my_alsgh@naver.com>
2023-02-05 20:22:19 +0900

    [FEAT] Notify DataSource Logic Modify

--- data/src/main/java/com/mument_android/data/datasource/notify/NotifyDataSourceImpl.kt
+++ data/src/main/java/com/mument_android/data/datasource/notify/NotifyDataSourceImpl.kt
@@ -1,9 +1,9 @@
 package com.mument_android.data.datasource.notify
 
+import android.util.Log
 import com.mument_android.data.dto.home.NotifyDto
+import com.mument_android.data.dto.home.RequestReadNotify
 import com.mument_android.data.network.home.NotifyService
-import com.mument_android.data.util.ResultWrapper
-import com.mument_android.data.util.catchingApiCall
 import retrofit2.Response
 import javax.inject.Inject
 
@@ -11,9 +11,16 @@ class NotifyDataSourceImpl @Inject constructor(private val notifyService: Notify
     NotifyDataSource {
     override suspend fun fetchNotifyList(): Response<NotifyDto> = notifyService.fetchNotifyList()
 
-    override suspend fun fetchNotifyRead(unreadNews: List<Int>): ResultWrapper<Void?> =
-        catchingApiCall { notifyService.fetchNotifyRead(unreadNews) }
+    override suspend fun fetchNotifyRead(unreadNews: List<Int>): Boolean? =
+        runCatching {
+            Log.e("fetchNotifyRead", unreadNews.toString())
+            notifyService.fetchNotifyRead(RequestReadNotify(unreadNews)).code() == 204
+        }.getOrElse {
+            null
+        }
 
-    override suspend fun fetchNotifyDelete(newsId: String): ResultWrapper<Void?> =
-        catchingApiCall { notifyService.fetchNotifyDelete(newsId) }
+    override suspend fun fetchNotifyDelete(newsId: String): Boolean? =
+        runCatching { notifyService.fetchNotifyDelete(newsId).code() == 200 }.getOrElse {
+            null
+        }
 }
