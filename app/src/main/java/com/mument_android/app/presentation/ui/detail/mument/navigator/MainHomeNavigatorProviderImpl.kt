3b128575d310eb2886e528aeec7fff469c489ac6
heaven00 <69586104+lhb8106@users.noreply.github.com>
2023-02-05 14:12:59 +0900

    [FEAT] #173 - 불필요한 코드 삭제

--- app/src/main/java/com/mument_android/app/presentation/ui/detail/mument/navigator/MainHomeNavigatorProviderImpl.kt
+++ app/src/main/java/com/mument_android/app/presentation/ui/detail/mument/navigator/MainHomeNavigatorProviderImpl.kt
@@ -1,7 +1,6 @@
 package com.mument_android.app.presentation.ui.detail.mument.navigator
 
 import android.app.Activity
-import android.content.Context
 import android.content.Intent
 import com.angdroid.navigation.MainHomeNavigatorProvider
 import com.mument_android.app.presentation.ui.main.MainActivity
