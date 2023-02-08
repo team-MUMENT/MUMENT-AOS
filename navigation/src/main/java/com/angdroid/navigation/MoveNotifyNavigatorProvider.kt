a535d1a54d9a054e9b39608460ae808efbba63f3
minho <my_alsgh@naver.com>
2023-02-08 22:26:15 +0900

    [FEAT] Notify Navigator Add

---
+++ navigation/src/main/java/com/angdroid/navigation/MoveNotifyNavigatorProvider.kt
@@ -0,0 +1,8 @@
+package com.angdroid.navigation
+
+import com.mument_android.domain.entity.music.MusicInfoEntity
+
+interface MoveNotifyNavigatorProvider {
+    fun moveToNoticeDetail(noticeId: Int)
+    fun moveToMumentDetail(mumentId: String, musicInfoEntity: MusicInfoEntity)
+}
