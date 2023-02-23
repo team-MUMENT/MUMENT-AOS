b5184a1a300a34a9276a75b1e790c13b47b38e6b
heaven00 <69586104+lhb8106@users.noreply.github.com>
2023-02-23 23:40:44 +0900

    [FEAT] #234 mainActivity 종료 실패

--- app/src/main/java/com/mument_android/app/presentation/ui/detail/mument/navigator/QuitMainNavigatorProviderImpl.kt
+++ app/src/main/java/com/mument_android/app/presentation/ui/detail/mument/navigator/QuitMainNavigatorProviderImpl.kt
@@ -15,10 +15,10 @@ class QuitMainNavigatorProviderImpl @Inject constructor(
 ): QuitMainNavigatorProvider {
     override fun quitMument() {
         val intent = Intent(activity, LogInActivity::class.java)
-        intent.flags = Intent.FLAG_ACTIVITY_CLEAR_TASK or Intent.FLAG_ACTIVITY_NEW_TASK
+        intent.flags = Intent.FLAG_ACTIVITY_CLEAR_TOP or Intent.FLAG_ACTIVITY_SINGLE_TOP or Intent.FLAG_ACTIVITY_NEW_TASK or Intent.FLAG_ACTIVITY_CLEAR_TASK
+        intent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK)
         activity.finishAffinity()
         activity.startActivity(intent)
         exitProcess(0)
-
     }
 }
