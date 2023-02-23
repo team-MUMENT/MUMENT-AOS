49eaab30970aa5eb931ecbafd1c48aa119d9abe4
minho <my_alsgh@naver.com>
2023-02-23 18:50:26 +0900

    [FIX] Complete Music Detail AntiPattern..

--- feature/detail/src/main/java/com/mument_android/detail/mument/listener/MumentClickListener.kt
+++ feature/detail/src/main/java/com/mument_android/detail/mument/listener/MumentClickListener.kt
@@ -1,7 +1,5 @@
 package com.mument_android.detail.mument.listener
 
-import com.mument_android.domain.entity.music.MusicInfoEntity
-
 interface MumentClickListener {
     fun showMumentDetail(mumentId: String)
     fun likeMument(mumentId: String)
