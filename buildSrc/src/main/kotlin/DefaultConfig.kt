0344afeb184b4795c524746958d52f0458d47675
heaven00 <69586104+lhb8106@users.noreply.github.com>
2023-03-27 01:17:29 +0900

    [FEAT] 릴리즈 전 수정

--- buildSrc/src/main/kotlin/DefaultConfig.kt
+++ buildSrc/src/main/kotlin/DefaultConfig.kt
@@ -3,8 +3,8 @@ object DefaultConfig {
     const val MIN_SDK = 27
     const val TARGET_SDK = 33
     const val COMPILE_SDK = 33
-    const val VERSION_CODE = 5
-    const val VERSION_NAME = "1.0.0"
+    const val VERSION_CODE = 7
+    const val VERSION_NAME = "1.0.1"
     const val JVM_TARGET = "1.8"
     const val TEST_INSTRUMENTATION_RUNNER = "androidx.test.runner.AndroidJUnitRunner"
 }
