fb0e58b166f6c84403b4fbbe81254ac71878cf75
minho <my_alsgh@naver.com>
2023-05-27 16:43:02 +0900

    [REFACTOR] DataSource Module Provides to Binds

--- data/src/main/java/com/mument_android/data/datasource/home/RemoteSearchListDataSourceImpl.kt
+++ data/src/main/java/com/mument_android/data/datasource/home/RemoteSearchListDataSourceImpl.kt
@@ -4,8 +4,9 @@ import com.mument_android.data.dto.home.RecentSearchDataDto
 import com.mument_android.data.network.home.HomeService
 import com.mument_android.data.util.ResultWrapper
 import com.mument_android.data.util.catchingApiCall
+import javax.inject.Inject
 
-class RemoteSearchListDataSourceImpl(private val homeService: HomeService) : RemoteSearchListDataSource {
+class RemoteSearchListDataSourceImpl @Inject constructor(private val homeService: HomeService) : RemoteSearchListDataSource {
     override suspend fun searchMusicList(keyword: String): ResultWrapper<RecentSearchDataDto?> =
         catchingApiCall { homeService.searchMusicList(keyword) }
 }
