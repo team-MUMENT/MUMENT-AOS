f57dddd66b87bc6c0f984a9c5f07e0dff0bf3aa6
minho <my_alsgh@naver.com>
2023-05-27 16:43:02 +0900

    [REFACTOR] DataSource Module Provides to Binds

--- data/src/main/java/com/mument_android/data/datasource/mypage/UserInfoDataSourceImpl.kt
+++ data/src/main/java/com/mument_android/data/datasource/mypage/UserInfoDataSourceImpl.kt
@@ -1,6 +1,5 @@
 package com.mument_android.data.datasource.mypage
 
-import com.mument_android.data.dto.mypage.BlockUserDto
 import com.mument_android.data.dto.mypage.UserInfoDto
 import com.mument_android.data.network.mypage.MyPageApiService
 import com.mument_android.data.util.BaseResponse
