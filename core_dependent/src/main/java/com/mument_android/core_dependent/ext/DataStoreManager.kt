9095d9e97327eb06e514347370bc93eb4be000a3
minho <my_alsgh@naver.com>
2023-05-27 16:45:38 +0900

    [REFACTOR] DataStore Inject 해당 클래스로 이관

--- core_dependent/src/main/java/com/mument_android/core_dependent/ext/DataStoreManager.kt
+++ core_dependent/src/main/java/com/mument_android/core_dependent/ext/DataStoreManager.kt
@@ -4,12 +4,17 @@ import android.content.Context
 import androidx.datastore.core.DataStore
 import androidx.datastore.preferences.core.*
 import androidx.datastore.preferences.preferencesDataStore
+import dagger.hilt.android.qualifiers.ApplicationContext
 import kotlinx.coroutines.flow.Flow
 import kotlinx.coroutines.flow.catch
 import kotlinx.coroutines.flow.map
 import java.io.IOException
+import javax.inject.Inject
+import javax.inject.Singleton
 
-class DataStoreManager(
+@Singleton
+class DataStoreManager @Inject constructor(
+    @ApplicationContext
     val context: Context
 ) {
     private val Context.datastore: DataStore<Preferences> by preferencesDataStore("mument_datastore")
