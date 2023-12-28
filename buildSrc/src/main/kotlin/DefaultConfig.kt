538a588aebeb889a643567044dde4d42021564f6
minho <kr1227kr@flow.team>
2023-12-28 15:33:46 +0900

    [BUILD] 1.0.3(9)

--- buildSrc/src/main/kotlin/DefaultConfig.kt
+++ buildSrc/src/main/kotlin/DefaultConfig.kt
@@ -3,8 +3,8 @@ object DefaultConfig {
     const val MIN_SDK = 27
     const val TARGET_SDK = 33
     const val COMPILE_SDK = 33
-    const val VERSION_CODE = 8
-    const val VERSION_NAME = "1.0.2"
+    const val VERSION_CODE = 9
+    const val VERSION_NAME = "1.0.3"
     const val JVM_TARGET = "1.8"
     const val TEST_INSTRUMENTATION_RUNNER = "androidx.test.runner.AndroidJUnitRunner"
 }
